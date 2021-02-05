83b4ae729d7ffa022214f89fd3192109a4f65654
Igor Demin <igor.demin@jetbrains.com>
2021-02-05 12:22:07 +0300

    Desktop. Change API for resource loading
    1. introduce painterResource, deprecate svgResource/vectorXmlResource/imageResource
    similar to https://android-review.googlesource.com/c/platform/frameworks/support/+/1561297
    
    2. rename:
    - loadSvgResource -> loadSvgPainter
    - loadVectorXmlResource -> loadXmlImageVector
    - imageFromResource -> loadImageBitmap
    
    3. add a public useResourceStream, so users can use it in combination with load* functions
    
    4. deprecate imageFromResource
    
    See also
    https://github.com/JetBrains/compose-jb/issues/259
    
    Test: ./gradlew jvmTest desktopTest -Pandroidx.compose.multiplatformEnabled=true
    Change-Id: I8da0dcde2190df0d5aa4080be9f3de71222ef742

--- compose/desktop/desktop/samples/src/jvmMain/kotlin/androidx/compose/desktop/examples/example1/Main.jvm.kt
+++ compose/desktop/desktop/samples/src/jvmMain/kotlin/androidx/compose/desktop/examples/example1/Main.jvm.kt
@@ -86,9 +86,7 @@ import androidx.compose.ui.input.key.plus
 import androidx.compose.ui.input.key.shortcuts
 import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.svgResource
-import androidx.compose.ui.res.vectorXmlResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
 import androidx.compose.ui.text.SpanStyle
@@ -136,7 +134,7 @@ private fun App() {
                     title = {
                         Row(verticalAlignment = Alignment.CenterVertically) {
                             Image(
-                                svgResource("androidx/compose/desktop/example/star.svg"),
+                                painterResource("androidx/compose/desktop/example/star.svg"),
                                 contentDescription = "Star"
                             )
                             Text(title)
@@ -470,13 +468,13 @@ private fun ScrollableContent(scrollState: ScrollState) {
 
         Row {
             Image(
-                imageResource("androidx/compose/desktop/example/circus.jpg"),
+                painterResource("androidx/compose/desktop/example/circus.jpg"),
                 "Localized description",
                 Modifier.size(200.dp)
             )
 
             Icon(
-                vectorXmlResource("androidx/compose/desktop/example/ic_baseline_deck_24.xml"),
+                painterResource("androidx/compose/desktop/example/ic_baseline_deck_24.xml"),
                 "Localized description",
                 Modifier.size(100.dp).align(Alignment.CenterVertically),
                 tint = Color.Blue.copy(alpha = 0.5f)
