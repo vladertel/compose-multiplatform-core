8a4f1d7b4c42cda60d66f0c2ce396858430fb82b
Andrey Rudenko <andrei.rudenko@jetbrains.com>
2021-06-17 11:53:44 +0200

    Desktop ContextMenu
    - ContextMenuProvider is available in composables. It makes two things: 1. adds to the stack new menu items 2. on desktop adds a right button click handler
    - SelectionContainer and CoreTextField use ContextMenuProvider and define menu items for copy / paste.
    - A state of the context menu is represented by ContextMenuState, where `status` var is available. It can be used both for changes state and react to changes
    - ContextMenu implementation is configurable via LocalComposition. This CL includes implementation for Material theme
    - very basic PlatformLocalization interface was added to allow customize default text labels
    - Current context menu items customization abilities are intentionally very limited: only text + callback. Addition options is a subject for future work
    
    Test: manual
    Change-Id: Ib93f44708001027fa727d6148edfacd676a9d259

---
+++ compose/desktop/desktop/src/jvmMain/kotlin/androidx/compose/desktop/ContextMenuRepresentation.desktop.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.desktop
+
+import androidx.compose.foundation.ContextMenuData
+import androidx.compose.foundation.ContextMenuItem
+import androidx.compose.foundation.ContextMenuRepresentation
+import androidx.compose.foundation.ContextMenuState
+import androidx.compose.material.CursorDropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.node.Ref
+
+internal val contextMenuRepresentation = object : ContextMenuRepresentation {
+    @Composable
+    override fun Representation(state: ContextMenuState, data: ContextMenuData) {
+        val isOpen = state.status is ContextMenuState.Status.Open
+        val previousItems = remember { Ref<List<ContextMenuItem>>() }
+        val items = if (isOpen) {
+            remember { data.itemsSeq.toList() }
+        } else {
+            previousItems.value ?: emptyList()
+        }
+        previousItems.value = items
+
+        CursorDropdownMenu(
+            expanded = state.status is ContextMenuState.Status.Open,
+            onDismissRequest = { state.status = ContextMenuState.Status.Closed }
+        ) {
+            items.forEach { item ->
+                DropdownMenuItem(
+                    onClick = {
+                        state.status = ContextMenuState.Status.Closed
+                        item.onClick()
+                    }
+                ) {
+                    Text(text = item.label)
+                }
+            }
+        }
+    }
+}
