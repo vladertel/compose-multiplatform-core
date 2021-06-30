184b0e5628fedde89c8e19927634ec91d44443ad
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-06-30 06:04:42 +0300

    Native demo with a Button

--- compose/native/demo/src/macosX64Main/kotlin/androidx/compose/native/demo/main.kt
+++ compose/native/demo/src/macosX64Main/kotlin/androidx/compose/native/demo/main.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.graphics.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 
-
 fun main() {
     NSApplication.sharedApplication()
     createWindow("Compose/Native sample")
@@ -23,53 +22,64 @@ fun main() {
 
 fun createWindow(title: String) {
     println("createWindow()")
-    /*val window = */
     Window(title) {
-        Box(modifier = Modifier
-            .padding(16.dp)
-            .background(color = Color.Red)
-            .width(100.dp).height(100.dp)
-            .clickable {
-                println("Red box: clicked")
-            }.pointerMoveFilter(
-                onMove = {
-                    println("Red box: onMove")
-                    true
-                },
-                onEnter = {
-                    println("Red box: onEnter")
-                    true
-                },
-                onExit = {
-                    println("Red box: onExit")
-                    true
-                }
-            )
-        ) {
+        Column {
             Box(modifier = Modifier
                 .padding(16.dp)
-                .background(color = Color.Blue)
-                .width(20.dp).height(20.dp)
+                .background(color = Color.Red)
+                .width(100.dp).height(100.dp)
                 .clickable {
-                    println("Blue box: clicked")
-                }
-                .pointerMoveFilter(
+                    println("Red box: clicked")
+                }.pointerMoveFilter(
                     onMove = {
-                        println("Blue box: onMove")
+                        println("Red box: onMove")
                         true
                     },
                     onEnter = {
-                        println("Blue box: onEnter")
+                        println("Red box: onEnter")
                         true
                     },
                     onExit = {
-                        println("Blue box: onExit")
+                        println("Red box: onExit")
                         true
                     }
                 )
+            ) {
+                Box(modifier = Modifier
+                    .padding(16.dp)
+                    .background(color = Color.Blue)
+                    .width(20.dp).height(20.dp)
+                    .clickable {
+                        println("Blue box: clicked")
+                    }
+                    .pointerMoveFilter(
+                        onMove = {
+                            println("Blue box: onMove")
+                            true
+                        },
+                        onEnter = {
+                            println("Blue box: onEnter")
+                            true
+                        },
+                        onExit = {
+                            println("Blue box: onExit")
+                            true
+                        }
+                    )
+                )
+            }
+            Spacer(
+                Modifier.width(200.dp).height(4.dp).background(color = Color.DarkGray)
             )
+            Button(
+                modifier = Modifier
+                    .padding(16.dp),
+                onClick = {
+                    println("HELLO again!")
+                }
+            ) {
+            }
         }
     }
     println("end createWindow()")
 }
-
