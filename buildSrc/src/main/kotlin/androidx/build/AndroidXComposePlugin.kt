touchlab-lab/androidx

Rebase upon the current jb-main
Change-Id: I67d0b46a31794931a84b78db47bf3df044855e8d

buildSrc/src/main/kotlin/androidx/build/AndroidXComposePlugin.kt
@@ -80,16 +80,18 @@ class AndroidXComposePlugin : Plugin<Project> {
                     }
 
                     project.tasks.withType(KotlinCompile::class.java).configureEach { compile ->
-                        // TODO(b/157230235): remove when this is enabled by default
-                        compile.kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-                        compile.inputs.files({ kotlinPlugin.files })
-                            .withPropertyName("composeCompilerExtension")
-                            .withNormalizer(ClasspathNormalizer::class.java)
-                        compile.doFirst {
-                            if (!conf.isEmpty) {
-                                compile.kotlinOptions.freeCompilerArgs +=
-                                    "-Xplugin=${kotlinPlugin.files.first()}"
-                            }
+                        if (compile !is KotlinNativeCompile) {
+                            // TODO(b/157230235): remove when this is enabled by default
+                            compile.kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+                                compile.inputs.files({ kotlinPlugin.files })
+                                .withPropertyName("composeCompilerExtension")
+                                .withNormalizer(ClasspathNormalizer::class.java)
+                                compile.doFirst {
+                                    if (!conf.isEmpty) {
+                                        compile.kotlinOptions.freeCompilerArgs +=
+                                            "-Xplugin=${kotlinPlugin.files.first()}"
+                                    }
+                                }
                         }
                     }
 

vladertel/compose-multiplatform-core

buildSrc/src/main/kotlin/androidx/build/AndroidXComposePlugin.kt
@@ -79,15 +79,17 @@ class AndroidXComposePlugin : Plugin<Project> {
                     }.files
 
                     project.tasks.withType(KotlinCompile::class.java).configureEach { compile ->
-                        // TODO(b/157230235): remove when this is enabled by default
-                        compile.kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-                        compile.inputs.files({ kotlinPlugin })
-                            .withPropertyName("composeCompilerExtension")
-                            .withNormalizer(ClasspathNormalizer::class.java)
-                        compile.doFirst {
-                            if (!kotlinPlugin.isEmpty) {
-                                compile.kotlinOptions.freeCompilerArgs +=
-                                    "-Xplugin=${kotlinPlugin.first()}"
+                        if (compile !is KotlinNativeCompile) {
+                            // TODO(b/157230235): remove when this is enabled by default
+                            compile.kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+                            compile.inputs.files({ kotlinPlugin })
+                                .withPropertyName("composeCompilerExtension")
+                                .withNormalizer(ClasspathNormalizer::class.java)
+                            compile.doFirst {
+                                if (!kotlinPlugin.isEmpty) {
+                                    compile.kotlinOptions.freeCompilerArgs +=
+                                        "-Xplugin=${kotlinPlugin.first()}"
+                                }
                             }
                         }
                     }