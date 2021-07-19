bce6c5a30fbd6c2d80d4e777d62e0161b6b92df5
Roman Sedaikin <roman.sedaikin@jetbrains.com>
2021-07-19 17:00:25 +0300

    Resizing an undecorated window feature.
    - added UndecoratedWindowResizer
    - ComposeWindow uses UndecoratedWindowResizer to resize when not decorated
    
    Test: manual.
    Change-Id: Ie3c202c8f2cab65b1fbef7ae50371de620928c1e

--- compose/ui/ui/src/desktopMain/kotlin/androidx/compose/desktop/ComposeWindow.desktop.kt
+++ compose/ui/ui/src/desktopMain/kotlin/androidx/compose/desktop/ComposeWindow.desktop.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionContext
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.window.UndecoratedWindowResizer
 import androidx.compose.ui.window.WindowPlacement
 import org.jetbrains.skiko.ClipComponent
 import org.jetbrains.skiko.GraphicsApi
@@ -117,6 +118,18 @@ class ComposeWindow : JFrame() {
             isDisposed = true
         }
         super.dispose()
+    }
+
+    private val undecoratedWindowResizer = UndecoratedWindowResizer(this)
+
+    override fun setUndecorated(value: Boolean) {
+        super.setUndecorated(value)
+        undecoratedWindowResizer.enabled = isUndecorated && isResizable
+    }
+
+    override fun setResizable(value: Boolean) {
+        super.setResizable(value)
+        undecoratedWindowResizer.enabled = isUndecorated && isResizable
     }
 
     override fun setVisible(value: Boolean) {
