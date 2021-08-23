6eeb8713ad9486ceca8cc02e75a036ce197c871b
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2021-08-23 15:23:54 +0200

    web: improve composable lambdas in js (#31)
    * web: experiment with ComposableLambda for kotlin/js
    
    Change-Id: I9d6f4d5ac676ccd085ac68957a9892fdc198b270
    
    * web: experiment with ComposableLambda for kotlin/js
    
    Change-Id: I0ef2e71abb9eba53d2c0173fc56fb522e8e76c2a
    
    * web: experiment with ComposableLambda for kotlin/js (ready for review/discussion)
    
    Change-Id: Ibbdb408e9997303d3210b2e0de08e16e7f80a447
    
    Co-authored-by: Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
    # Conflicts:
    #	compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposeIrGenerationExtension.kt

--- compose/runtime/runtime/src/test/kotlin/androidx/compose/runtime/RecomposerTests.kt
+++ compose/runtime/runtime/src/test/kotlin/androidx/compose/runtime/RecomposerTests.kt
@@ -113,7 +113,7 @@ class RecomposerTests {
     }
 
     @Test
-    @IgnoreJsTarget
+    //@IgnoreJsTarget
     fun testRecomposition() = compositionTest {
         val counter = Counter()
         val triggers = mapOf(
