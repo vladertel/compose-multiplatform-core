a6c9b6c4d13b94135dfc20d08cec3b4fabd3b482
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-08-28 18:32:25 +0200

    Make FixComposableLambdaCalls.kt consider more cases of composable lambda calls
    This commit fixes a bug that was breaking composable lambda calls taking anonymous lambdas as arguments
    
    Change-Id: Ibe978ab3b69be8d0c53cb365c56604417cf4ae44

--- compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/decoys/FixComposableLambdaCalls.kt
+++ compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/decoys/FixComposableLambdaCalls.kt
@@ -19,13 +19,16 @@ package androidx.compose.compiler.plugins.kotlin.lower.decoys
 import androidx.compose.compiler.plugins.kotlin.ModuleMetrics
 import androidx.compose.compiler.plugins.kotlin.lower.AbstractComposeLowering
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
 import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.declarations.IrValueParameter
+import org.jetbrains.kotlin.ir.declarations.impl.IrVariableImpl
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrConstKind
 import org.jetbrains.kotlin.ir.expressions.IrExpression
@@ -37,14 +40,20 @@ import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrElseBranchImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrIfThenElseImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrTypeOperatorCallImpl
+import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
+import org.jetbrains.kotlin.ir.symbols.impl.IrVariableSymbolImpl
 import org.jetbrains.kotlin.ir.types.IrSimpleType
 import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.types.makeNullable
 import org.jetbrains.kotlin.ir.util.DeepCopySymbolRemapper
 import org.jetbrains.kotlin.ir.util.defaultType
 import org.jetbrains.kotlin.ir.util.dump
 import org.jetbrains.kotlin.ir.util.isFunction
+import org.jetbrains.kotlin.ir.util.isLocal
+import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.platform.js.isJs
 import org.jetbrains.kotlin.resolve.BindingTrace
 
@@ -87,13 +96,36 @@ class FixComposableLambdaCalls(
 
     private val composerType = composerIrClass.defaultType.replaceArgumentsWithStarProjections()
 
-    private val composableLambdaClassImpl = symbolRemapper.getReferencedClass(context.referenceClass(
-        FqName("androidx.compose.runtime.internal.ComposableLambdaImpl")
-    )!!).owner
+    private val composableLambdaClassImpl = symbolRemapper.getReferencedClass(
+        context.referenceClass(
+            FqName("androidx.compose.runtime.internal.ComposableLambdaImpl")
+        )!!
+    ).owner
 
     override fun visitClass(declaration: IrClass): IrStatement {
         if (declaration == composableLambdaClassImpl) return declaration
         return super.visitClass(declaration)
+    }
+
+    private fun IrExpression.hasLocalFunForLambda(): Boolean {
+        var foundLocalFunctionForLambda = false
+        this.transformChildrenVoid(object : IrElementTransformerVoid() {
+            override fun visitElement(element: IrElement): IrElement {
+                if (foundLocalFunctionForLambda) {
+                    return element
+                }
+                return super.visitElement(element)
+            }
+
+            override fun visitFunction(declaration: IrFunction): IrStatement {
+                if (declaration.isLocal) {
+                    foundLocalFunctionForLambda = true
+                    return declaration
+                }
+                return super.visitFunction(declaration)
+            }
+        })
+        return foundLocalFunctionForLambda
     }
 
     override fun visitCall(expression: IrCall): IrExpression {
@@ -119,14 +151,6 @@ class FixComposableLambdaCalls(
             return original
         }
 
-        val targetInvoke = composableLambdaClassImpl.declarations.firstOrNull {
-            it is IrFunction
-                && it.name.asString() == "invoke"
-                && it.valueParameters.size == original.valueArgumentsCount
-        } as? IrSimpleFunction ?: error(
-            "ComposableLambdaImpl.invoke() not found " + original.dump()
-        )
-
         // There are cases when dispatchReceiver is not of ComposableLambdaImpl type, so
         // we need to add a type check like below at runtime:
         // if (content is ComposableLambdaImpl)
@@ -134,36 +158,95 @@ class FixComposableLambdaCalls(
         // else {
         //     content(composer, 14 & changed)
         // }
-        return IrIfThenElseImpl(
-            UNDEFINED_OFFSET, UNDEFINED_OFFSET,
-            context.irBuiltIns.unitType
-        ).apply {
-            branches.add(IrBranchImpl(
-                // if (content is ComposableLambdaImpl)
-                condition = IrTypeOperatorCallImpl(
-                    startOffset = UNDEFINED_OFFSET,
-                    endOffset = UNDEFINED_OFFSET,
-                    type = context.irBuiltIns.booleanType,
-                    operator = IrTypeOperator.INSTANCEOF,
-                    typeOperand = composableLambdaClassImpl.defaultType,
-                    argument = dispatchReceiver
-                ),
-                // then: content.invoke(composer, 14 & changed)
-                result = IrCallImpl(
-                    startOffset = UNDEFINED_OFFSET,
-                    endOffset = UNDEFINED_OFFSET,
-                    type = original.type,
-                    symbol = targetInvoke.symbol,
-                    typeArgumentsCount = 0,
-                    valueArgumentsCount = original.valueArgumentsCount,
-                    origin = null
+        val hoistedArguments = mutableListOf<IrVariableImpl>()
+        val realArguments = mutableListOf<IrExpression?>()
+
+        // hoist the dispatchReceiver expression into a variable to avoid
+        // dispatchReceiver expression being called multiple times
+        val dispatchReceiverVar = IrVariableImpl(
+            UNDEFINED_OFFSET,
+            UNDEFINED_OFFSET,
+            IrDeclarationOrigin.DEFINED,
+            IrVariableSymbolImpl(),
+            Name.identifier("\$invokeComposable_dispatchReceiver"),
+            type = original.dispatchReceiver!!.type,
+            isVar = false,
+            isConst = false,
+            isLateinit = false
+        ).also {
+            it.initializer = original.dispatchReceiver
+        }
+
+        // Hoist argument expressions to avoid anonymous lambdas declarations duplicates
+        // in if..then..else branches.
+        repeat(original.valueArgumentsCount) {
+            val arg = original.getValueArgument(it)
+            if (arg != null && arg.hasLocalFunForLambda()) {
+                val hoistedArg = IrVariableImpl(
+                    UNDEFINED_OFFSET,
+                    UNDEFINED_OFFSET,
+                    IrDeclarationOrigin.DEFINED,
+                    IrVariableSymbolImpl(),
+                    Name.identifier("\$hoistedArg_$${hoistedArguments.size}"),
+                    type = arg.type.makeNullable(),
+                    isVar = false,
+                    isConst = false,
+                    isLateinit = false
                 ).also {
-                    it.dispatchReceiver = dispatchReceiver
-                    repeat(original.valueArgumentsCount) { ix ->
-                        it.putValueArgument(ix, original.getValueArgument(ix))
-                    }
+                    it.initializer = arg
                 }
-            ))
+                hoistedArguments.add(hoistedArg)
+                irGet(hoistedArg)
+            } else {
+                arg
+            }.also {  realArgExpr ->
+                realArguments.add(realArgExpr)
+            }
+        }
+
+        fun actualCall(actualSymbol: IrSimpleFunctionSymbol): IrCall {
+            return IrCallImpl(
+                startOffset = UNDEFINED_OFFSET,
+                endOffset = UNDEFINED_OFFSET,
+                type = original.type,
+                symbol = actualSymbol,
+                typeArgumentsCount = 0,
+                valueArgumentsCount = original.valueArgumentsCount,
+                origin = null
+            ).also {
+                it.dispatchReceiver = irGet(dispatchReceiverVar)
+                repeat(original.valueArgumentsCount) { ix ->
+                    it.putValueArgument(ix, realArguments[ix])
+                }
+            }
+        }
+
+        val conditionalInvoke = IrIfThenElseImpl(
+            UNDEFINED_OFFSET, UNDEFINED_OFFSET,
+            original.type
+        ).apply {
+            val targetInvoke = composableLambdaClassImpl.declarations.firstOrNull {
+                it is IrFunction
+                    && it.name.asString() == "invoke"
+                    && it.valueParameters.size == original.valueArgumentsCount
+            } as? IrSimpleFunction ?: error(
+                "ComposableLambdaImpl.invoke() not found " + original.dump()
+            )
+            branches.add(
+                IrBranchImpl(
+                    // if (content is ComposableLambdaImpl)
+                    condition = IrTypeOperatorCallImpl(
+                        startOffset = UNDEFINED_OFFSET,
+                        endOffset = UNDEFINED_OFFSET,
+                        type = context.irBuiltIns.booleanType,
+                        operator = IrTypeOperator.INSTANCEOF,
+                        typeOperand = composableLambdaClassImpl.defaultType,
+                        argument = irGet(dispatchReceiverVar)
+                    ),
+                    // then: content.invoke(composer, 14 & changed)
+                    result = actualCall(actualSymbol = targetInvoke.symbol)
+                )
+            )
             branches.add(
                 // else: content(composer, 14 & changed)
                 IrElseBranchImpl(
@@ -174,9 +257,17 @@ class FixComposableLambdaCalls(
                         IrConstKind.Boolean,
                         true
                     ),
-                    result = expression
+                    result = actualCall(actualSymbol = original.symbol)
                 )
             )
         }
+        return irBlock(
+            type = expression.type,
+            statements = mutableListOf<IrStatement>().apply {
+                add(dispatchReceiverVar)
+                addAll(hoistedArguments)
+                add(conditionalInvoke)
+            }
+        )
     }
 }
