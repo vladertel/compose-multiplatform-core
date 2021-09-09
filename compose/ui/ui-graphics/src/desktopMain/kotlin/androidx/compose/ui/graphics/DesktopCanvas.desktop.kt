touchlab-lab/androidx

Extended native Owner and Owners Lots of skija to skia/native conversion for native ui/graphics
Change-Id: I4fbca3e9b60f73f685c5fece067c1910c17a829f

compose/ui/ui-graphics/src/desktopMain/kotlin/androidx/compose/ui/graphics/DesktopCanvas.desktop.kt
@@ -35,7 +35,7 @@ import org.jetbrains.skija.ClipMode as SkijaClipMode
 import org.jetbrains.skija.RRect as SkijaRRect
 import org.jetbrains.skija.Rect as SkijaRect
 
-actual typealias NativeCanvas = org.jetbrains.skija.Canvas
+actual typealias PlatformCanvas = org.jetbrains.skija.Canvas
 
 internal actual fun ActualCanvas(image: ImageBitmap): Canvas {
     val skijaBitmap = image.asDesktopBitmap()
@@ -45,7 +45,7 @@ internal actual fun ActualCanvas(image: ImageBitmap): Canvas {
     return DesktopCanvas(org.jetbrains.skija.Canvas(skijaBitmap))
 }
 
-actual val Canvas.nativeCanvas: NativeCanvas get() = (this as DesktopCanvas).skija
+actual val Canvas.nativeCanvas: PlatformCanvas get() = (this as DesktopCanvas).skija
 
 class DesktopCanvas(val skija: org.jetbrains.skija.Canvas) : Canvas {
     private val Paint.skija get() = (this as DesktopPaint).skija

compose/ui/ui-graphics/src/desktopMain/kotlin/androidx/compose/ui/graphics/DesktopPaint.desktop.kt
@@ -20,14 +20,14 @@ import org.jetbrains.skija.PaintMode as SkijaPaintMode
 import org.jetbrains.skija.PaintStrokeCap as SkijaPaintStrokeCap
 import org.jetbrains.skija.PaintStrokeJoin as SkijaPaintStrokeJoin
 
-actual typealias NativePaint = org.jetbrains.skija.Paint
+actual typealias PlatformPaint = org.jetbrains.skija.Paint
 
 actual fun Paint(): Paint = DesktopPaint()
 
 class DesktopPaint : Paint {
     internal val skija = org.jetbrains.skija.Paint()
 
-    override fun asFrameworkPaint(): NativePaint = skija
+    override fun asFrameworkPaint(): PlatformPaint = skija
 
     override var alpha: Float
         get() = Color(skija.color).alpha

vladertel/compose-multiplatform-core

compose/ui/ui-graphics/src/desktopMain/kotlin/androidx/compose/ui/graphics/DesktopCanvas.desktop.kt
@@ -35,7 +35,7 @@ import org.jetbrains.skia.ClipMode as SkiaClipMode
 import org.jetbrains.skia.RRect as SkiaRRect
 import org.jetbrains.skia.Rect as SkiaRect
 
-actual typealias NativeCanvas = org.jetbrains.skia.Canvas
+actual typealias PlatformCanvas = org.jetbrains.skia.Canvas
 
 internal actual fun ActualCanvas(image: ImageBitmap): Canvas {
     val skiaBitmap = image.asDesktopBitmap()
@@ -45,7 +45,7 @@ internal actual fun ActualCanvas(image: ImageBitmap): Canvas {
     return DesktopCanvas(org.jetbrains.skia.Canvas(skiaBitmap))
 }
 
-actual val Canvas.nativeCanvas: NativeCanvas get() = (this as DesktopCanvas).skia
+actual val Canvas.nativeCanvas: PlatformCanvas get() = (this as DesktopCanvas).skia
 
 class DesktopCanvas(val skia: org.jetbrains.skia.Canvas) : Canvas {
     private val Paint.skia get() = (this as DesktopPaint).skia

compose/ui/ui-graphics/src/desktopMain/kotlin/androidx/compose/ui/graphics/DesktopPaint.desktop.kt
@@ -20,14 +20,14 @@ import org.jetbrains.skia.PaintMode as SkiaPaintMode
 import org.jetbrains.skia.PaintStrokeCap as SkiaPaintStrokeCap
 import org.jetbrains.skia.PaintStrokeJoin as SkiaPaintStrokeJoin
 
-actual typealias NativePaint = org.jetbrains.skia.Paint
+actual typealias PlatformPaint = org.jetbrains.skia.Paint
 
 actual fun Paint(): Paint = DesktopPaint()
 
 class DesktopPaint : Paint {
     internal val skia = org.jetbrains.skia.Paint()
 
-    override fun asFrameworkPaint(): NativePaint = skia
+    override fun asFrameworkPaint(): PlatformPaint = skia
 
     override var alpha: Float
         get() = Color(skia.color).alpha