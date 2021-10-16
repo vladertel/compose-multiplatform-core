b7f5300355d430f2131b353332a455bc18f81ad0
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-10-16 04:34:18 +0300

    Adapted buildSrc to native build

--- buildSrc/private/src/main/kotlin/androidx/build/AndroidXComposeImplPlugin.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/AndroidXComposeImplPlugin.kt
@@ -33,9 +33,9 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.type.ArtifactTypeDefinition
 import org.gradle.api.attributes.Attribute
 import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.api.tasks.ClasspathNormalizer
 import org.gradle.api.artifacts.ExternalModuleDependency
-import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.api.attributes.Usage
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.apply
@@ -124,9 +124,11 @@ class AndroidXComposeImplPlugin : Plugin<Project> {
                 project.configureForKotlinMultiplatformSourceStructure()
             }
 
-            project.tasks.withType(KotlinCompile::class.java).configureEach { compile ->
-                // Needed to enable `expect` and `actual` keywords
-                compile.kotlinOptions.freeCompilerArgs += "-Xmulti-platform"
+            project.afterEvaluate { projectAfterEvaluate ->
+                projectAfterEvaluate.tasks.withType(KotlinCompile::class.java).configureEach { compile ->
+                    // Needed to enable `expect` and `actual` keywords
+                    compile.kotlinOptions.freeCompilerArgs += "-Xmulti-platform"
+                }
             }
 
             project.tasks.withType(KotlinJsCompile::class.java).configureEach { compile ->
@@ -371,7 +373,6 @@ fun Project.configureComposeImplPluginForAndroidx() {
                             "$composeMetricsOption=${metricsDest.absolutePath}"
                         )
                 }
-
                 // since metrics reports in compose compiler are a new feature, we only want to
                 // pass in this parameter for modules that are using the tip of tree compose
                 // compiler, or else we will run into an exception since the parameter will not
@@ -389,21 +390,19 @@ fun Project.configureComposeImplPluginForAndroidx() {
         }
     }
 
-    project.afterEvaluate {
-        val androidXExtension =
-            project.extensions.findByType(AndroidXExtension::class.java)
-        if (androidXExtension != null) {
-            if (androidXExtension.publish.shouldPublish()) {
-                project.tasks.withType(KotlinCompile::class.java)
-                    .configureEach { compile ->
-                        compile.doFirst {
-                            if (!kotlinPlugin.isEmpty) {
-                                compile.kotlinOptions.freeCompilerArgs +=
-                                    listOf("-P", composeSourceOption)
-                            }
+    val androidXExtension =
+        project.extensions.findByType(AndroidXExtension::class.java)
+    if (androidXExtension != null) {
+        if (androidXExtension.publish.shouldPublish()) {
+            project.tasks.withType(KotlinCompile::class.java)
+                .configureEach { compile ->
+                    compile.doFirst {
+                        if (!kotlinPlugin.isEmpty) {
+                            compile.kotlinOptions.freeCompilerArgs +=
+                                listOf("-P", composeSourceOption)
                         }
                     }
-            }
+                }
         }
     }
 }
