c76718ce8369662fb0d3ce40f9282089bbcfa242
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-10-16 03:34:18 +0200

    [compilation] compose native commits (squashed) by a.gorshenev
    [ui/ui] mpp branch rebase artifacts
    
    [ui/ui] actual synchronized
    
    Moved CI to JetBrains/compose-jb/mpp branch
    
    Change-Id: I26d1633a661c0bdddff7c932312307387acaa708
    
    Allow native mobile runtime tests
    
    [runtime] allow compose:runtime:runtime:macosX64Test to pass
    
    Don't pass -Xplugin with common plugin  to native compulations
    
    Change-Id: I874f17d7211a6a25c6ad5ca5f053ef6a32d75c36
    
    [js runtime] Added some common actuals because of native
    
    [ui/ui] adapted to the recent rebase
    
    [native demo build.gradle] native demo build.gradle for native-mobile
    
    [native demo] Extended native demo to native-mobile
    
    [ui/ui] changes for native-mobile
    
    [compose/native/build.gradle] updated for native-mobile
    
    [build.gradle] updated build.gradle files for native-mobile
    
    Created compose/native/demo
    
    [ui/ui] initial support of skiko mouse events
    
    [ui/ui] darwinMain
    
    [ui/ui] @ThreadLocal attributes for several companion objects
    
    multithreading and actuals for jvm in runtime
    
    animation, foundation  and material actuals
    
    atomic reference
    
    Created a dummy syncronized.
    Limited to ui/ui needs for now
    
    [ui/ui] native actuals
    
    [ui/ui-text] native actuals
    
    [ui/ui-graphics] native actuals
    
    native runtime: threading actuals
    
    native compose runtime: invokeComposable, ComposableLambda
    
    [ui/ui, compose/native] Moved ComposeLayer, ComposeWindow, Window to ui/ui
    
    Created compose/native
    
    Introduced native source sets in build.gradle files
    
    Adapted buildSrc to native build
    
    Change-Id: Ibf0d8bc649819cbf8e2f03fe728ec2a151564262

--- compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/NodeCoordinator.kt
+++ compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/NodeCoordinator.kt
@@ -1186,6 +1186,7 @@ internal abstract class NodeCoordinator(
         )
     }
 
+    @kotlin.native.concurrent.ThreadLocal
     internal companion object {
         const val ExpectAttachedLayoutCoordinates = "LayoutCoordinate operations are only valid " +
             "when isAttached is true"
