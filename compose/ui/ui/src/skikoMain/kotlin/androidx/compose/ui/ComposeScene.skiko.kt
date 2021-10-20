1c916feaccf37ee0b6df6c1e8a46d3fde0f1b088
Igor Demin <igor.demin@jetbrains.com>
2021-10-20 16:43:36 +0300

    Desktop. Pass scroll delta to Compose (new API)
    Test: ./gradlew jvmTest desktopTest -Pandroidx.compose.multiplatformEnabled=true
    Change-Id: Ifab91869ec518ae069dcecaf3a77ab72665eb01a
    
    # Conflicts:
    #	compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/ComposeScene.desktop.kt
    #	compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/awt/ComposeLayer.desktop.kt
    #	compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/input/pointer/PointerEvent.desktop.kt
    #	compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/ComposeScene.skiko.kt

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/ComposeScene.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/ComposeScene.skiko.kt
@@ -32,9 +32,6 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.key.KeyEvent as ComposeKeyEvent
-import androidx.compose.ui.input.mouse.MouseScrollEvent
-import androidx.compose.ui.input.mouse.MouseScrollOrientation
-import androidx.compose.ui.input.mouse.MouseScrollUnit
 import androidx.compose.ui.input.pointer.PointerButtons
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.PointerInputEvent
@@ -372,6 +369,7 @@ class ComposeScene internal constructor(
      *
      * @param eventType Indicates the primary reason that the event was sent.
      * @param position The [Offset] of the current pointer event, relative to the content.
+     * @param scrollDelta scroll delta for the PointerEventType.Scroll event
      * @param timeMillis The time of the current pointer event, in milliseconds. The start (`0`) time
      * is platform-dependent.
      * @param type The device type that produced the event, such as [mouse][PointerType.Mouse],
@@ -385,6 +383,7 @@ class ComposeScene internal constructor(
     fun sendPointerEvent(
         eventType: PointerEventType,
         position: Offset,
+        scrollDelta: Offset = Offset(0f, 0f),
         timeMillis: Long = System.nanoTime() / 1_000_000L,
         type: PointerType = PointerType.Mouse,
         buttons: PointerButtons? = null,
@@ -404,6 +403,7 @@ class ComposeScene internal constructor(
             nativeEvent,
             type,
             pointerId,
+            scrollDelta,
             actualButtons,
             actualKeyboardModifiers
         )
@@ -421,49 +421,12 @@ class ComposeScene internal constructor(
             }
             PointerEventType.Enter -> hoveredOwner?.processPointerInput(event)
             PointerEventType.Exit -> hoveredOwner?.processPointerInput(event)
+            PointerEventType.Scroll -> hoveredOwner?.processPointerInput(event)
         }
 
         if (!actualButtons.areAnyPressed) {
             mousePressOwner = null
         }
-    }
-
-    // TODO(demin): remove/change when we will have scroll event support in the common code
-    // TODO(demin): return Boolean (when it is consumed).
-    //  see ComposeLayer todo about AWTDebounceEventQueue
-    /**
-     * Send pointer scroll event to the content.
-     *
-     * @param position The [Offset] of the current pointer event, relative to the content
-     * @param delta Change of mouse scroll.
-     * Positive if scrolling down, negative if scrolling up.
-     * @param orientation Orientation in which scrolling event occurs.
-     * Up/down wheel scrolling causes events in vertical orientation.
-     * Left/right wheel scrolling causes events in horizontal orientation.
-     * @param timeMillis The time of the current pointer event, in milliseconds. The start (`0`) time
-     * is platform-dependent.
-     * @param type The device type that produced the event, such as [mouse][PointerType.Mouse],
-     * or [touch][PointerType.Touch].
-     * @param buttons Contains the state of pointer buttons (e.g. mouse and stylus buttons).
-     * @param keyboardModifiers Contains the state of modifier keys, such as Shift, Control, and Alt, as well as the state
-     * of the lock keys, such as Caps Lock and Num Lock.
-     * @param nativeEvent The original native event
-     */
-    @OptIn(ExperimentalComposeUiApi::class)
-    @Suppress("UNUSED_PARAMETER")
-    @ExperimentalComposeUiApi // it is more experimental than ComposeScene itself
-    fun sendPointerScrollEvent(
-        position: Offset,
-        delta: MouseScrollUnit,
-        orientation: MouseScrollOrientation = MouseScrollOrientation.Vertical,
-        timeMillis: Long = System.nanoTime() / 1_000_000L,
-        type: PointerType = PointerType.Mouse,
-        buttons: PointerButtons? = null,
-        keyboardModifiers: PointerKeyboardModifiers? = null,
-        nativeEvent: Any? = null,
-    ): Unit = postponeInvalidation {
-        check(!isClosed) { "ComposeScene is closed" }
-        hoveredOwner?.onMouseScroll(position, MouseScrollEvent(delta, orientation))
     }
 
     private fun onMousePressed(event: PointerInputEvent) {
@@ -524,6 +487,7 @@ internal expect fun pointerInputEvent(
     nativeEvent: Any?,
     type: PointerType,
     pointerId: Long,
+    scrollDelta: Offset,
     buttons: PointerButtons,
     keyboardModifiers: PointerKeyboardModifiers,
 ): PointerInputEvent
