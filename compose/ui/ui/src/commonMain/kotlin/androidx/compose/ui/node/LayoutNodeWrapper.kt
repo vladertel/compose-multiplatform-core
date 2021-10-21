c91b085490bfc64c4650748650589fcc33cc245a
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-10-21 06:20:27 +0300

    [ui/ui] @ThreadLocal attributes for several companion objects

--- compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/LayoutNodeWrapper.kt
+++ compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/LayoutNodeWrapper.kt
@@ -1112,6 +1112,7 @@ internal abstract class LayoutNodeWrapper(
         )
     }
 
+    @kotlin.native.concurrent.ThreadLocal
     internal companion object {
         const val ExpectAttachedLayoutCoordinates = "LayoutCoordinate operations are only valid " +
             "when isAttached is true"
