52bd79d12f0c629ecddcc38f0b663a75fea17ce8
Igor Demin <igordmn@gmail.com>
2021-11-10 20:39:29 +0300

    Hide cursor if window isn't focused
    All desktop platforms do it, and it is an user friendly behaviour, as it indicates - there is no active text field, in which we currently type.

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/TextFieldCursor.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/TextFieldCursor.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.isUnspecified
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.Density
@@ -49,7 +50,8 @@ internal fun Modifier.cursor(
 ) = if (enabled) composed {
     val cursorAlpha = remember { Animatable(1f) }
     val isBrushSpecified = !(cursorBrush is SolidColor && cursorBrush.value.isUnspecified)
-    if (state.hasFocus && value.selection.collapsed && isBrushSpecified) {
+    val isWindowFocused = LocalWindowInfo.current.isWindowFocused
+    if (state.hasFocus && value.selection.collapsed && isBrushSpecified && isWindowFocused) {
         LaunchedEffect(cursorBrush, value.annotatedString, value.selection) {
             cursorAlpha.animateTo(0f, cursorAnimationSpec)
         }
