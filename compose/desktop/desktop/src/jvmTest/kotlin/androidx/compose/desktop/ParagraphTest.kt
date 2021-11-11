841e62ce6e76cd98a2c0d8b95fa182451a864233
Igor Demin <igordmn@gmail.com>
2021-11-11 11:40:23 +0100

    ImageComposeScene
    Desktop. Deprecate TestComposeWindow in favor to ImageComposeScene
    
    This CL introduces ImageComposeScene - simplified version of ComposeScene, with drawing only into Image.
    
    It is almost the same as the old TestComposeWindow, but with a different class and method names.
    
    See examples of usage in ImageComposeSceneTest
    
    I "renamed" TestComposeWindow to ImageComposeScene, because its name can confuse people:
    1. because we had "Window" in the name, some people thought that it creates the real native window
    2. "Test" in the name can mislead about the purpose of the class. The main purpose, of course - is to use it in screenshot tests, but we can also use it in PDF generation, for example.
    
    Also, we introduce:
    - renderComposeScene - one-liner function to draw Composable content into an image
    - ImageComposeScene.use - similar to AutoClosable.use to disposing created resources. We don't inherit AutoCloseable, because we want to move ImageComposeScene into skikoMain. For now, common code doesn't have Closeable, but in the future it can (https://github.com/Kotlin/kotlinx-io/blob/c5bba0114e33c2afd8e4b1ab214aad94ee416d00/core/commonMain/src/kotlinx/io/Closeable.common.kt#L7), so I renamed `dispose` method to `close`
    
    Also I have fixed a race when we call ComposeScene.render from another thread and use TestCoroutineDispatcher or Dispatcher.Unconfined (see FlushCoroutineDispatcher and ComposeScene).
    The race was between:
    ```
    [Swing thread] GlobalSnapshotManager -> sendApplyNotifications -> (ComposeScene.recomposer) awaitWorkAvailable, ..., parentFrameClock.withFrameNanos
    ```
    and
    ```
    [Test thread] ComposeScene.render -> sendApplyNotifications, ..., frameClock.sendFrame
    ```
    In the Swing thread recomposer still don't wait for the frame, but in test thread we already send it.
    To make sure that recomposer waiting for the frame, we had `dispatcher.flush`.
    But it don't wait for the already running tasks (in our case - for sendApplyNotifications to finish in Swing thread).
    In this CL we fix that, adding a synchronization point - we don't exit `flush` if there are still performing tasks.
    
    Test: ./gradlew jvmTest desktopTest -Pandroidx.compose.multiplatformEnabled=true
    Test: manual ./gradlew :compose:desktop:desktop:desktop-samples:run1 -Pandroidx.compose.multiplatformEnabled=true
    
    Change-Id: I48d2769fd2eaff53e391371aea9ed37964b6afc3
    
    # Conflicts:
    # compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/ComposeScene.desktop.kt
    # compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/platform/CoroutineDispatchers.desktop.kt
    # compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/platform/FlushCoroutineDispatcher.desktop.kt
    # compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/CoroutineDispatchers.skiko.kt
    
    Mark `use` as experimental
    
    Fix TestComposeWindow
    
    ComposeScene doesn't flush LaunchedEffect's, but TestComposeWindow can't work without it, and can't work with Dispatchers.Unconfined

--- compose/desktop/desktop/src/jvmTest/kotlin/androidx/compose/desktop/ParagraphTest.kt
+++ compose/desktop/desktop/src/jvmTest/kotlin/androidx/compose/desktop/ParagraphTest.kt
@@ -26,10 +26,11 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.TestComposeWindow
 import androidx.compose.ui.test.InternalTestApi
+import androidx.compose.ui.renderComposeScene
 import androidx.compose.ui.test.junit4.DesktopScreenshotTestRule
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -79,9 +80,7 @@ class ParagraphTest {
     @Ignore
     @Test
     fun paragraphBasics() {
-        val window = TestComposeWindow(width = 1024, height = 768)
-
-        window.setContent {
+        val snapshot = renderComposeScene(width = 1024, height = 768) {
             ProvideTextStyle(TextStyle(fontFamily = fontFamily)) {
                 Column(Modifier.fillMaxSize().background(Color.White), Arrangement.SpaceEvenly) {
                     Text(
@@ -121,6 +120,6 @@ class ParagraphTest {
                 }
             }
         }
-        screenshotRule.snap(window.surface)
+        screenshotRule.write(snapshot)
     }
 }
--- compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/input/mouse/MouseHoverFilterTest.kt
+++ compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/input/mouse/MouseHoverFilterTest.kt
@@ -17,15 +17,20 @@
 package androidx.compose.ui.input.mouse
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.ImageComposeScene
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.TestComposeWindow
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.use
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -33,15 +38,17 @@ import org.junit.runners.JUnit4
 @OptIn(ExperimentalComposeUiApi::class)
 @RunWith(JUnit4::class)
 class MouseHoverFilterTest {
-    private val window = TestComposeWindow(width = 100, height = 100, density = Density(2f))
-
     @Test
-    fun `inside window`() {
+    fun `inside window`() = ImageComposeScene(
+        width = 100,
+        height = 100,
+        density = Density(2f)
+    ).use { scene ->
         var moveCount = 0
         var enterCount = 0
         var exitCount = 0
 
-        window.setContent {
+        scene.setContent {
             Box(
                 modifier = Modifier
                     .pointerMove(
@@ -58,42 +65,35 @@ class MouseHoverFilterTest {
                     .size(10.dp, 20.dp)
             )
         }
-        window.onMouseEntered(
-            x = 0,
-            y = 0
-        )
-        window.onMouseMoved(
-            x = 10,
-            y = 20
-        )
+
+        scene.sendPointerEvent(PointerEventType.Enter, Offset(0f, 0f))
+        scene.sendPointerEvent(PointerEventType.Move, Offset(10f, 20f))
         assertThat(enterCount).isEqualTo(1)
         assertThat(exitCount).isEqualTo(0)
         assertThat(moveCount).isEqualTo(1)
 
-        window.onMouseMoved(
-            x = 10,
-            y = 15
-        )
+        scene.sendPointerEvent(PointerEventType.Move, Offset(10f, 15f))
         assertThat(enterCount).isEqualTo(1)
         assertThat(exitCount).isEqualTo(0)
         assertThat(moveCount).isEqualTo(2)
 
-        window.onMouseMoved(
-            x = 30,
-            y = 30
-        )
+        scene.sendPointerEvent(PointerEventType.Move, Offset(30f, 30f))
         assertThat(enterCount).isEqualTo(1)
         assertThat(exitCount).isEqualTo(1)
         assertThat(moveCount).isEqualTo(2)
     }
 
     @Test
-    fun `window enter`() {
+    fun `window enter`() = ImageComposeScene(
+        width = 100,
+        height = 100,
+        density = Density(2f)
+    ).use { scene ->
         var moveCount = 0
         var enterCount = 0
         var exitCount = 0
 
-        window.setContent {
+        scene.setContent {
             Box(
                 modifier = Modifier
                     .pointerMove(
@@ -111,34 +111,124 @@ class MouseHoverFilterTest {
             )
         }
 
-        window.onMouseEntered(
-            x = 10,
-            y = 20
-        )
+        scene.sendPointerEvent(PointerEventType.Enter, Offset(10f, 20f))
         assertThat(enterCount).isEqualTo(1)
         assertThat(exitCount).isEqualTo(0)
         assertThat(moveCount).isEqualTo(0)
 
-        window.onMouseExited()
+        scene.sendPointerEvent(PointerEventType.Exit, Offset(-1f, -1f))
         assertThat(enterCount).isEqualTo(1)
         assertThat(exitCount).isEqualTo(1)
         assertThat(moveCount).isEqualTo(0)
     }
-}
 
-private fun Modifier.pointerMove(
-    onMove: () -> Unit,
-    onExit: () -> Unit,
-    onEnter: () -> Unit,
-): Modifier = pointerInput(onMove, onExit, onEnter) {
-    awaitPointerEventScope {
-        while (true) {
-            val event = awaitPointerEvent()
-            when (event.type) {
-                PointerEventType.Move -> onMove()
-                PointerEventType.Enter -> onEnter()
-                PointerEventType.Exit -> onExit()
+    @Test
+    fun `move from one component to another`() = ImageComposeScene(
+        width = 100,
+        height = 100,
+        density = Density(1f)
+    ).use { scene ->
+        var enterCount1 = 0
+        var exitCount1 = 0
+        var enterCount2 = 0
+        var exitCount2 = 0
+
+        scene.setContent {
+            Column {
+                Box(
+                    modifier = Modifier
+                        .pointerMove(
+                            onEnter = { enterCount1++ },
+                            onExit = { exitCount1++ }
+                        )
+                        .size(10.dp, 10.dp)
+                )
+                Box(
+                    modifier = Modifier
+                        .pointerMove(
+                            onEnter = { enterCount2++ },
+                            onExit = { exitCount2++ }
+                        )
+                        .size(10.dp, 10.dp)
+                )
             }
+        }
+
+        scene.sendPointerEvent(PointerEventType.Enter, Offset(5f, 5f))
+        assertThat(enterCount1).isEqualTo(1)
+        assertThat(exitCount1).isEqualTo(0)
+        assertThat(enterCount2).isEqualTo(0)
+        assertThat(exitCount2).isEqualTo(0)
+
+        scene.sendPointerEvent(PointerEventType.Move, Offset(5f, 15f))
+        assertThat(enterCount1).isEqualTo(1)
+        assertThat(exitCount1).isEqualTo(1)
+        assertThat(enterCount2).isEqualTo(1)
+        assertThat(exitCount2).isEqualTo(0)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `send multiple move events with paused dispatcher`() {
+        var moveCount = 0
+        var enterCount = 0
+        var exitCount = 0
+
+        val dispatcher = TestCoroutineDispatcher().apply {
+            pauseDispatcher()
+        }
+
+        ImageComposeScene(
+            width = 100,
+            height = 100,
+            density = Density(1f),
+            coroutineContext = dispatcher
+        ).use { scene ->
+            scene.setContent {
+                Box(
+                    modifier = Modifier
+                        .pointerMove(
+                            onMove = {
+                                moveCount++
+                            },
+                            onEnter = {
+                                enterCount++
+                            },
+                            onExit = {
+                                exitCount++
+                            }
+                        )
+                        .size(20.dp, 20.dp)
+                )
+            }
+            dispatcher.advanceUntilIdle()
+
+            scene.sendPointerEvent(PointerEventType.Enter, Offset(5f, 10f))
+            scene.sendPointerEvent(PointerEventType.Move, Offset(6f, 10f))
+            scene.sendPointerEvent(PointerEventType.Move, Offset(7f, 10f))
+            scene.sendPointerEvent(PointerEventType.Press, Offset(9f, 10f))
+            dispatcher.advanceUntilIdle()
+
+            assertThat(enterCount).isEqualTo(1)
+            assertThat(exitCount).isEqualTo(0)
+            assertThat(moveCount).isEqualTo(2)
+
+            scene.sendPointerEvent(PointerEventType.Move, Offset(100f, 10f))
+            dispatcher.advanceUntilIdle()
+
+            assertThat(enterCount).isEqualTo(1)
+            assertThat(exitCount).isEqualTo(1)
+            assertThat(moveCount).isEqualTo(2)
         }
     }
 }
+
+@OptIn(ExperimentalComposeUiApi::class)
+private fun Modifier.pointerMove(
+    onMove: () -> Unit = {},
+    onExit: () -> Unit = {},
+    onEnter: () -> Unit = {},
+): Modifier = this
+    .onPointerEvent(PointerEventType.Move) { onMove() }
+    .onPointerEvent(PointerEventType.Exit) { onExit() }
+    .onPointerEvent(PointerEventType.Enter) { onEnter() }
