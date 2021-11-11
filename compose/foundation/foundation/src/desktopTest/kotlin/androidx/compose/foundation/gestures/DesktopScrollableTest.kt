1b3f506f53c9afb53caba959c654ca40421b2307
Igor Demin <igordmn@gmail.com>
2021-11-11 11:40:23 +0100

    ImageComposeScene
    Desktop. Deprecate TestComposeWindow in favor to ImageComposeScene
    
    This CL introduces ImageComposeScene - simplified version of ComposeScene, with drawing only into Image.
    
    It is almost the same as the old TestComposeWindow, but with a different class and method names.
    
    See examples of usage in ImageComposeSceneTest
    
    I "renamed" TestComposeWindow to ImageComposeScene, because its name can confuse people:
    1. because we had "Window" in the name, some people thought that it creates the real native window
    2. "Test" in the name can mislead about the purpose of the class. The main purpose, of course - is to use it in screenshot tests, but we can also use it in PDF generation, for example.
    
    Also, we introduce:
    - renderComposeScene - one-liner function to draw Composable content into an image
    - ImageComposeScene.use - similar to AutoClosable.use to disposing created resources. We don't inherit AutoCloseable, because we want to move ImageComposeScene into skikoMain. For now, common code doesn't have Closeable, but in the future it can (https://github.com/Kotlin/kotlinx-io/blob/c5bba0114e33c2afd8e4b1ab214aad94ee416d00/core/commonMain/src/kotlinx/io/Closeable.common.kt#L7), so I renamed `dispose` method to `close`
    
    Also I have fixed a race when we call ComposeScene.render from another thread and use TestCoroutineDispatcher or Dispatcher.Unconfined (see FlushCoroutineDispatcher and ComposeScene).
    The race was between:
    ```
    [Swing thread] GlobalSnapshotManager -> sendApplyNotifications -> (ComposeScene.recomposer) awaitWorkAvailable, ..., parentFrameClock.withFrameNanos
    ```
    and
    ```
    [Test thread] ComposeScene.render -> sendApplyNotifications, ..., frameClock.sendFrame
    ```
    In the Swing thread recomposer still don't wait for the frame, but in test thread we already send it.
    To make sure that recomposer waiting for the frame, we had `dispatcher.flush`.
    But it don't wait for the already running tasks (in our case - for sendApplyNotifications to finish in Swing thread).
    In this CL we fix that, adding a synchronization point - we don't exit `flush` if there are still performing tasks.
    
    Test: ./gradlew jvmTest desktopTest -Pandroidx.compose.multiplatformEnabled=true
    Test: manual ./gradlew :compose:desktop:desktop:desktop-samples:run1 -Pandroidx.compose.multiplatformEnabled=true
    
    Change-Id: I48d2769fd2eaff53e391371aea9ed37964b6afc3
    
    # Conflicts:
    # compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/ComposeScene.desktop.kt
    # compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/platform/CoroutineDispatchers.desktop.kt
    # compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/platform/FlushCoroutineDispatcher.desktop.kt
    # compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/CoroutineDispatchers.skiko.kt
    
    Mark `use` as experimental
    
    Fix TestComposeWindow
    
    ComposeScene doesn't flush LaunchedEffect's, but TestComposeWindow can't work without it, and can't work with Dispatchers.Unconfined

--- compose/foundation/foundation/src/desktopTest/kotlin/androidx/compose/foundation/gestures/DesktopScrollableTest.kt
+++ compose/foundation/foundation/src/desktopTest/kotlin/androidx/compose/foundation/gestures/DesktopScrollableTest.kt
@@ -16,19 +16,20 @@
 
 package androidx.compose.foundation.gestures
 
+import androidx.compose.foundation.awtWheelEvent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.ImageComposeScene
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.mouse.MouseScrollEvent
-import androidx.compose.ui.input.mouse.MouseScrollOrientation
-import androidx.compose.ui.input.mouse.MouseScrollUnit
-import androidx.compose.ui.platform.TestComposeWindow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.use
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -40,23 +41,20 @@ import kotlin.math.sqrt
 class DesktopScrollableTest {
     private val density = 2f
 
-    private fun window() = TestComposeWindow(
-        width = 100,
-        height = 100,
-        density = Density(density)
-    )
-
     private fun scrollLineLinux(bounds: Dp) = sqrt(bounds.value * density)
     private fun scrollLineWindows(bounds: Dp) = bounds.value * density / 20f
     private fun scrollLineMacOs() = density * 10f
     private fun scrollPage(bounds: Dp) = bounds.value * density
 
     @Test
-    fun `linux, scroll vertical`() {
-        val window = window()
+    fun `linux, scroll vertical`() = ImageComposeScene(
+        width = 100,
+        height = 100,
+        density = Density(density)
+    ).use { scene ->
         val context = TestColumn()
 
-        window.setContent {
+        scene.setContent {
             CompositionLocalProvider(
                 LocalMouseScrollConfig provides MouseScrollableConfig.LinuxGnome
             ) {
@@ -71,29 +69,34 @@ class DesktopScrollableTest {
             }
         }
 
-        window.onMouseScroll(
-            x = 0,
-            y = 0,
-            event = MouseScrollEvent(MouseScrollUnit.Line(3f), MouseScrollOrientation.Vertical)
+        scene.sendPointerEvent(
+            eventType = PointerEventType.Scroll,
+            position = Offset.Zero,
+            scrollDelta = Offset(0f, 3f),
+            nativeEvent = awtWheelEvent(),
         )
 
         assertThat(context.offset).isWithin(0.1f).of(-3 * scrollLineLinux(20.dp))
 
-        window.onMouseScroll(
-            x = 0,
-            y = 0,
-            event = MouseScrollEvent(MouseScrollUnit.Line(3f), MouseScrollOrientation.Vertical)
+        scene.sendPointerEvent(
+            eventType = PointerEventType.Scroll,
+            position = Offset.Zero,
+            scrollDelta = Offset(0f, 3f),
+            nativeEvent = awtWheelEvent(),
         )
 
         assertThat(context.offset).isWithin(0.1f).of(-6 * scrollLineLinux(20.dp))
     }
 
     @Test
-    fun `windows, scroll vertical`() {
-        val window = window()
+    fun `windows, scroll vertical`() = ImageComposeScene(
+        width = 100,
+        height = 100,
+        density = Density(density)
+    ).use { scene ->
         val context = TestColumn()
 
-        window.setContent {
+        scene.setContent {
             CompositionLocalProvider(
                 LocalMouseScrollConfig provides MouseScrollableConfig.WindowsWinUI
             ) {
@@ -108,29 +111,34 @@ class DesktopScrollableTest {
             }
         }
 
-        window.onMouseScroll(
-            x = 0,
-            y = 0,
-            event = MouseScrollEvent(MouseScrollUnit.Line(-2f), MouseScrollOrientation.Vertical)
+        scene.sendPointerEvent(
+            eventType = PointerEventType.Scroll,
+            position = Offset.Zero,
+            scrollDelta = Offset(0f, -2f),
+            nativeEvent = awtWheelEvent(),
         )
 
         assertThat(context.offset).isWithin(0.1f).of(2 * scrollLineWindows(20.dp))
 
-        window.onMouseScroll(
-            x = 0,
-            y = 0,
-            event = MouseScrollEvent(MouseScrollUnit.Line(4f), MouseScrollOrientation.Vertical)
+        scene.sendPointerEvent(
+            eventType = PointerEventType.Scroll,
+            position = Offset.Zero,
+            scrollDelta = Offset(0f, 4f),
+            nativeEvent = awtWheelEvent(),
         )
 
         assertThat(context.offset).isWithin(0.1f).of(-2 * scrollLineWindows(20.dp))
     }
 
     @Test
-    fun `windows, scroll one page vertical`() {
-        val window = window()
+    fun `windows, scroll one page vertical`() = ImageComposeScene(
+        width = 100,
+        height = 100,
+        density = Density(density)
+    ).use { scene ->
         val context = TestColumn()
 
-        window.setContent {
+        scene.setContent {
             CompositionLocalProvider(
                 LocalMouseScrollConfig provides MouseScrollableConfig.WindowsWinUI
             ) {
@@ -145,21 +153,25 @@ class DesktopScrollableTest {
             }
         }
 
-        window.onMouseScroll(
-            x = 0,
-            y = 0,
-            event = MouseScrollEvent(MouseScrollUnit.Page(1f), MouseScrollOrientation.Vertical)
+        scene.sendPointerEvent(
+            eventType = PointerEventType.Scroll,
+            position = Offset.Zero,
+            scrollDelta = Offset(0f, 1f),
+            nativeEvent = awtWheelEvent(isScrollByPages = true),
         )
 
         assertThat(context.offset).isWithin(0.1f).of(-scrollPage(20.dp))
     }
 
     @Test
-    fun `macOS, scroll vertical`() {
-        val window = window()
+    fun `macOS, scroll vertical`() = ImageComposeScene(
+        width = 100,
+        height = 100,
+        density = Density(density)
+    ).use { scene ->
         val context = TestColumn()
 
-        window.setContent {
+        scene.setContent {
             CompositionLocalProvider(
                 LocalMouseScrollConfig provides MouseScrollableConfig.MacOSCocoa
             ) {
@@ -174,21 +186,25 @@ class DesktopScrollableTest {
             }
         }
 
-        window.onMouseScroll(
-            x = 0,
-            y = 0,
-            event = MouseScrollEvent(MouseScrollUnit.Line(-5.5f), MouseScrollOrientation.Vertical)
+        scene.sendPointerEvent(
+            eventType = PointerEventType.Scroll,
+            position = Offset.Zero,
+            scrollDelta =  Offset(0f, -5.5f),
+            nativeEvent = awtWheelEvent(),
         )
 
         assertThat(context.offset).isWithin(0.1f).of(5.5f * scrollLineMacOs())
     }
 
     @Test
-    fun `scroll with different orientation`() {
-        val window = window()
+    fun `scroll with different orientation`() = ImageComposeScene(
+        width = 100,
+        height = 100,
+        density = Density(density)
+    ).use { scene ->
         val column = TestColumn()
 
-        window.setContent {
+        scene.setContent {
             CompositionLocalProvider(
                 LocalMouseScrollConfig provides MouseScrollableConfig.LinuxGnome
             ) {
@@ -203,10 +219,11 @@ class DesktopScrollableTest {
             }
         }
 
-        window.onMouseScroll(
-            x = 0,
-            y = 0,
-            event = MouseScrollEvent(MouseScrollUnit.Line(3f), MouseScrollOrientation.Horizontal)
+        scene.sendPointerEvent(
+            eventType = PointerEventType.Scroll,
+            position = Offset.Zero,
+            scrollDelta =  Offset(3f, 0f),
+            nativeEvent = awtWheelEvent(),
         )
 
         assertThat(column.offset).isEqualTo(0f)
