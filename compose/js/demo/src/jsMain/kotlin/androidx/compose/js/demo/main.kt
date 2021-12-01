773af8570f48b1f1a1d0cb81e8f8d0e869b163bf
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-12-01 21:23:30 +0100

    update demo from mpp branch
    Change-Id: I833bf99684fdbabedd894bf9e0da9d58a1343f2c

--- compose/js/demo/src/jsMain/kotlin/androidx/compose/js/demo/main.kt
+++ compose/js/demo/src/jsMain/kotlin/androidx/compose/js/demo/main.kt
@@ -1,15 +1,31 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Switch
 import androidx.compose.js.ComposeWindow
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.unit.dp
-import kotlinx.browser.document
-import androidx.compose.foundation.background
 import org.jetbrains.skiko.wasm.onWasmReady
+import kotlinx.browser.document
 
 fun main() {
     val red = mutableStateOf(false)
@@ -17,15 +33,118 @@ fun main() {
     onWasmReady {
         ComposeWindow().setContent {
             println("setContent lambda is running")
-            Column {
-                Box(modifier = Modifier.width(100.dp).height(100.dp)
-                    .background(if (red.value) Color.Red else Color.Blue)
-                )
-            }
+            myContent()
+//            Column {
+//                Box(modifier = Modifier.width(100.dp).height(100.dp)
+//                    .background(if (red.value) Color.Red else Color.Blue)
+//                )
+//            }
         }
     }
 
     document.getElementById(elementId = "checkRed")!!.addEventListener("change", {
         red.value = !red.value
     })
+}
+
+@Composable
+fun myContent() {
+    var tick by remember { mutableStateOf(false) }
+    var selected by remember { mutableStateOf(false) }
+    var clutz by remember { mutableStateOf(false) }
+    var switched by remember { mutableStateOf(false) }
+    Column {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .background(color = if (selected) Color.Gray else Color.Red)
+                .width(100.dp).height(100.dp)
+                .clickable {
+                    println("Red box: clicked")
+                }.pointerMoveFilter(
+                    onMove = {
+                        println("Red box: onMove")
+                        true
+                    },
+                    onEnter = {
+                        println("Red box: onEnter")
+                        true
+                    },
+                    onExit = {
+                        println("Red box: onExit")
+                        true
+                    }
+                )
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .background(color = if (tick) Color.Green else Color.Blue)
+                    .width(20.dp).height(20.dp)
+                    .clickable {
+                        println("Small box: clicked")
+                    }
+                    .pointerMoveFilter(
+                        onMove = {
+                            println("Small box: onMove")
+                            true
+                        },
+                        onEnter = {
+                            println("Small box: onEnter")
+                            true
+                        },
+                        onExit = {
+                            println("Small box: onExit")
+                            true
+                        }
+                    )
+            )
+        }
+        Spacer(
+            Modifier.width(200.dp)
+                .height(if (clutz) 4.dp else 12.dp)
+                .background(color = if (clutz) Color.DarkGray else Color.Magenta)
+        )
+        // TODO: button fails in browser in runtime - needs investigation
+//        Button(
+//            modifier = Modifier
+//                .padding(16.dp),
+//            onClick = {
+//                println("Button clicked!")
+//                tick = !tick
+//            }
+//        ) {
+//            Text(if (switched) "🦑 press 🐙" else "Press me!")
+//        }
+        Row {
+            RadioButton(
+                modifier = Modifier
+                    .padding(16.dp),
+                selected = selected,
+                onClick = {
+                    println("RadioButton clicked!")
+                    selected = !selected
+                }
+            )
+
+            Checkbox(
+                checked = clutz,
+                modifier = Modifier.padding(16.dp),
+                onCheckedChange = { clutz = !clutz }
+            )
+        }
+        Switch(
+            modifier = Modifier
+                .padding(16.dp),
+            checked = switched,
+            onCheckedChange = { switched = it }
+        )
+    }
+    LaunchedEffect(Unit) {
+        while (true) {
+            withFrameNanos {
+                // println("NANO: $it, tick: $tick")
+            }
+        }
+    }
 }
