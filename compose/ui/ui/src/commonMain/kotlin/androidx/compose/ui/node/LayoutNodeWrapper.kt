2cbd3c1b3d0bb6a9d655cd2e4d4905dfa666778a
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-12-13 11:08:20 +0100

    mpp: revert back `(Canvas) -> Unit` type in method parameters
    Change-Id: Ic158b9a3474202de74292a9e041dd2478fba7ce3

--- compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/LayoutNodeWrapper.kt
+++ compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/LayoutNodeWrapper.kt
@@ -143,6 +143,10 @@ internal abstract class LayoutNodeWrapper(
     override val providedAlignmentLines: Set<AlignmentLine>
         get() = _measureResult?.alignmentLines?.keys ?: emptySet()
 
+    // it's necessary to keep this instance for k/js
+    // to avoid lambda instance being recreated every time
+    private val invokeOnCanvasInstance: (Canvas) -> Unit = { canvas -> this(canvas) }
+
     /**
      * Called when the width or height of [measureResult] change. The object instance pointed to
      * by [measureResult] may or may not have changed.
@@ -313,7 +317,7 @@ internal abstract class LayoutNodeWrapper(
         if (isAttached && layerBlock != null) {
             if (layer == null) {
                 layer = layoutNode.requireOwner().createLayer(
-                    this,
+                    invokeOnCanvasInstance,
                     invalidateParentLayer
                 ).apply {
                     resize(measuredSize)
