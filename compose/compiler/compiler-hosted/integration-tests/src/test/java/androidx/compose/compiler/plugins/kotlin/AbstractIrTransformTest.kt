touchlab-lab/androidx

Setup for DecoyTests
Change-Id: I6b417e40e18e0660580f7ebbdf975fee835b95c7

compose/compiler/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/AbstractIrTransformTest.kt
@@ -23,9 +23,18 @@ import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContextImpl
 import org.jetbrains.kotlin.backend.common.ir.BuiltinSymbolsBase
 import org.jetbrains.kotlin.backend.common.ir.createParameterDeclarations
+import org.jetbrains.kotlin.backend.common.serialization.mangle.ManglerChecker
+import org.jetbrains.kotlin.backend.common.serialization.mangle.descriptor.Ir2DescriptorManglerAdapter
+import org.jetbrains.kotlin.backend.common.serialization.metadata.DynamicTypeDeserializer
+import org.jetbrains.kotlin.backend.common.serialization.signature.IdSignatureDescriptor
 import org.jetbrains.kotlin.backend.jvm.JvmGeneratorExtensions
 import org.jetbrains.kotlin.backend.jvm.JvmNameProvider
 import org.jetbrains.kotlin.backend.jvm.serialization.JvmIdSignatureDescriptor
+import org.jetbrains.kotlin.builtins.KotlinBuiltIns
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.cli.js.messageCollectorLogger
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.cli.jvm.config.addJvmClasspathRoots
@@ -33,9 +42,18 @@ import org.jetbrains.kotlin.config.JVMConfigurationKeys
 import org.jetbrains.kotlin.config.JvmTarget
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
 import org.jetbrains.kotlin.descriptors.konan.DeserializedKlibModuleOrigin
 import org.jetbrains.kotlin.descriptors.konan.KlibModuleOrigin
+import org.jetbrains.kotlin.incremental.components.LookupTracker
 import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.backend.js.TopDownAnalyzerFacadeForJSIR
+import org.jetbrains.kotlin.ir.backend.js.emptyLoggingContext
+import org.jetbrains.kotlin.ir.backend.js.isBuiltIns
+import org.jetbrains.kotlin.ir.backend.js.jsResolveLibraries
+import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsIrLinker
+import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsManglerDesc
+import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsManglerIr
 import org.jetbrains.kotlin.ir.backend.jvm.serialization.EmptyLoggingContext
 import org.jetbrains.kotlin.ir.backend.jvm.serialization.JvmIrLinker
 import org.jetbrains.kotlin.ir.backend.jvm.serialization.JvmManglerDesc
@@ -45,6 +63,7 @@ import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.declarations.impl.IrFactoryImpl
+import org.jetbrains.kotlin.ir.declarations.persistent.PersistentIrFactory
 import org.jetbrains.kotlin.ir.descriptors.IrBuiltIns
 import org.jetbrains.kotlin.ir.descriptors.IrFunctionFactory
 import org.jetbrains.kotlin.ir.linkage.IrDeserializer
@@ -54,14 +73,24 @@ import org.jetbrains.kotlin.ir.util.ReferenceSymbolTable
 import org.jetbrains.kotlin.ir.util.SymbolTable
 import org.jetbrains.kotlin.ir.util.TypeTranslator
 import org.jetbrains.kotlin.ir.util.dump
+import org.jetbrains.kotlin.ir.visitors.acceptVoid
+import org.jetbrains.kotlin.js.config.ErrorTolerancePolicy
+import org.jetbrains.kotlin.js.config.JSConfigurationKeys
+import org.jetbrains.kotlin.konan.util.KlibMetadataFactories
+import org.jetbrains.kotlin.library.KotlinLibrary
+import org.jetbrains.kotlin.library.impl.isKotlinLibrary
+import org.jetbrains.kotlin.library.resolver.KotlinLibraryResolveResult
+import org.jetbrains.kotlin.library.resolver.TopologicalLibraryOrder
 import org.jetbrains.kotlin.load.kotlin.JvmPackagePartSource
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi2ir.Psi2IrConfiguration
 import org.jetbrains.kotlin.psi2ir.Psi2IrTranslator
 import org.jetbrains.kotlin.psi2ir.generators.GeneratorContext
+import org.jetbrains.kotlin.psi2ir.generators.GeneratorExtensions
 import org.jetbrains.kotlin.resolve.AnalyzingUtils
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedContainerSource
+import org.jetbrains.kotlin.storage.LockBasedStorageManager
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import java.io.File
 
@@ -492,6 +521,173 @@ abstract class AbstractIrTransformTest : AbstractCodegenTest() {
             irLinker.postProcess()
             return irModuleFragment
         }
+    }
+
+    inner class JsCompilation(private val verifySignatures: Boolean = true) : Compilation {
+        private val classpath = System.getProperty("androidx.compose.js.classpath").orEmpty()
+            .split(":")
+            .map { File(it) }
+            .filter { isKotlinLibrary(it) }
+
+        override val enabled: Boolean = classpath.isNotEmpty()
+
+        override fun compile(files: List<KtFile>): IrModuleFragment {
+            val configuration = newConfiguration()
+            val dependencyFiles = classpath.map { it.absolutePath }
+            configuration.put(JSConfigurationKeys.LIBRARIES, dependencyFiles)
+
+            val environment = KotlinCoreEnvironment.createForTests(
+                myTestRootDisposable,
+                configuration,
+                EnvironmentConfigFiles.JS_CONFIG_FILES
+            )
+            setupEnvironment(environment)
+
+            val analyzer = AnalyzerWithCompilerReport(environment.configuration)
+            val deps = jsResolveLibraries(
+                dependencyFiles,
+                emptyList(),
+                messageCollectorLogger(
+                    environment.configuration[CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY]!! as MessageCollector
+                )
+            )
+
+            val moduleProvider = JsModuleProvider(environment, deps)
+
+            analyzer.analyzeAndReport(files) {
+                TopDownAnalyzerFacadeForJSIR.analyzeFiles(
+                    files,
+                    environment.project,
+                    environment.configuration,
+                    deps.getFullList().map { moduleProvider.getModuleDescriptor(it) },
+                    friendModuleDescriptors = emptyList(),
+                    thisIsBuiltInsModule = false,
+                    customBuiltInsModule = moduleProvider.builtInsModule
+                )
+            }
+
+            val result = analyzer.analysisResult
+            TopDownAnalyzerFacadeForJSIR.checkForErrors(
+                files,
+                result.bindingContext,
+                ErrorTolerancePolicy.NONE
+            )
+            val mangler = JsManglerDesc
+            val signaturer = IdSignatureDescriptor(mangler)
+
+            val psi2Ir = Psi2IrTranslator(
+                configuration.languageVersionSettings,
+                Psi2IrConfiguration(),
+            )
+
+            val symbolTable = SymbolTable(signaturer, PersistentIrFactory)
+
+            val generatorContext = psi2Ir.createGeneratorContext(
+                result.moduleDescriptor,
+                result.bindingContext,
+                symbolTable,
+                GeneratorExtensions()
+            )
+
+            val irBuiltIns = generatorContext.irBuiltIns
+            val functionFactory = IrFunctionFactory(irBuiltIns, generatorContext.symbolTable)
+            irBuiltIns.functionFactory = functionFactory
+
+            val irLinker = JsIrLinker(
+                generatorContext.moduleDescriptor,
+                emptyLoggingContext,
+                generatorContext.irBuiltIns,
+                generatorContext.symbolTable,
+                functionFactory,
+                JsIrLinker.JsFePluginContext(
+                    result.moduleDescriptor,
+                    result.bindingContext,
+                    generatorContext.symbolTable,
+                    generatorContext.typeTranslator,
+                    generatorContext.irBuiltIns
+                ),
+            )
+
+            deps.getFullList(TopologicalLibraryOrder).forEach {
+                irLinker.deserializeOnlyHeaderModule(moduleProvider.getModuleDescriptor(it), it)
+            }
+
+            val builtinSymbolsBase = BuiltinSymbolsBase(
+                irBuiltIns,
+                irBuiltIns.builtIns,
+                generatorContext.symbolTable
+            )
+
+            psi2Ir.addPostprocessingStep {
+                postProcessingStep(it, generatorContext, irLinker, builtinSymbolsBase)
+            }
+
+            val moduleFragment = psi2Ir.generateModuleFragment(
+                generatorContext,
+                files,
+                listOf(irLinker),
+                emptyList(),
+            )
+
+            if (verifySignatures) {
+                moduleFragment.acceptVoid(
+                    ManglerChecker(JsManglerIr, Ir2DescriptorManglerAdapter(JsManglerDesc))
+                )
+            }
+
+            irLinker.postProcess()
+
+            return moduleFragment
+        }
+    }
+
+    private class JsModuleProvider(
+        private val environment: KotlinCoreEnvironment,
+        private val deps: KotlinLibraryResolveResult
+    ) {
+        private val JsFactories = KlibMetadataFactories(
+            { object : KotlinBuiltIns(it) {} },
+            DynamicTypeDeserializer
+        )
+
+        var builtInsModule: ModuleDescriptorImpl? = null
+        val descriptors = mutableMapOf<KotlinLibrary, ModuleDescriptorImpl>()
+        private val storageManager: LockBasedStorageManager =
+            LockBasedStorageManager("ModulesStructure")
+
+        private val moduleDependencies: Map<KotlinLibrary, List<KotlinLibrary>> = run {
+            val result = mutableMapOf<KotlinLibrary, List<KotlinLibrary>>()
+
+            deps.forEach { klib, _ ->
+                val dependencies = deps.filterRoots { it.library == klib }.getFullList(
+                    TopologicalLibraryOrder
+                )
+                result[klib] = dependencies.minus(klib)
+            }
+            result
+        }
+
+        fun getModuleDescriptor(current: KotlinLibrary): ModuleDescriptorImpl =
+            if (current in descriptors) {
+                descriptors.getValue(current)
+            } else {
+                JsFactories.DefaultDeserializedDescriptorFactory.createDescriptorOptionalBuiltIns(
+                    current,
+                    environment.configuration.languageVersionSettings,
+                    storageManager,
+                    builtInsModule?.builtIns,
+                    packageAccessHandler = null,
+                    lookupTracker = LookupTracker.DO_NOTHING
+                ).also { md ->
+                    descriptors[current] = md
+                    if (current.isBuiltIns) builtInsModule = md
+                    md.setDependencies(
+                        listOf(md) + moduleDependencies.getValue(current).map {
+                            getModuleDescriptor(it)
+                        }
+                    )
+                }
+            }
     }
 
     // This interface enables different Compilation variants for compiler tests

vladertel/compose-multiplatform-core

compose/compiler/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/AbstractIrTransformTest.kt
@@ -25,8 +25,18 @@ import org.jetbrains.kotlin.backend.common.ir.BuiltinSymbolsBase
 import org.jetbrains.kotlin.backend.common.ir.createParameterDeclarations
 import org.jetbrains.kotlin.backend.jvm.JvmGeneratorExtensionsImpl
 import org.jetbrains.kotlin.backend.jvm.JvmIrTypeSystemContext
+import org.jetbrains.kotlin.backend.common.serialization.mangle.ManglerChecker
+import org.jetbrains.kotlin.backend.common.serialization.mangle.descriptor.Ir2DescriptorManglerAdapter
+import org.jetbrains.kotlin.backend.common.serialization.metadata.DynamicTypeDeserializer
+import org.jetbrains.kotlin.backend.common.serialization.signature.IdSignatureDescriptor
+import org.jetbrains.kotlin.backend.jvm.JvmGeneratorExtensions
 import org.jetbrains.kotlin.backend.jvm.JvmNameProvider
 import org.jetbrains.kotlin.backend.jvm.serialization.JvmIdSignatureDescriptor
+import org.jetbrains.kotlin.builtins.KotlinBuiltIns
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.cli.js.messageCollectorLogger
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.cli.jvm.config.addJvmClasspathRoots
@@ -34,11 +44,21 @@ import org.jetbrains.kotlin.config.JVMConfigurationKeys
 import org.jetbrains.kotlin.config.JvmTarget
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
 import org.jetbrains.kotlin.descriptors.konan.DeserializedKlibModuleOrigin
 import org.jetbrains.kotlin.descriptors.konan.KlibModuleOrigin
 import org.jetbrains.kotlin.ir.IrBuiltIns
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.backend.jvm.serialization.JvmDescriptorMangler
+import org.jetbrains.kotlin.incremental.components.LookupTracker
+import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.backend.js.TopDownAnalyzerFacadeForJSIR
+import org.jetbrains.kotlin.ir.backend.js.isBuiltIns
+import org.jetbrains.kotlin.ir.backend.js.jsResolveLibraries
+import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsIrLinker
+import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsManglerDesc
+import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsManglerIr
+import org.jetbrains.kotlin.ir.backend.jvm.serialization.EmptyLoggingContext
 import org.jetbrains.kotlin.ir.backend.jvm.serialization.JvmIrLinker
 import org.jetbrains.kotlin.ir.builders.TranslationPluginContext
 import org.jetbrains.kotlin.ir.builders.declarations.buildClass
@@ -46,20 +66,33 @@ import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.declarations.impl.IrFactoryImpl
+import org.jetbrains.kotlin.ir.declarations.persistent.PersistentIrFactory
+import org.jetbrains.kotlin.ir.descriptors.IrBuiltIns
+import org.jetbrains.kotlin.ir.descriptors.IrFunctionFactory
 import org.jetbrains.kotlin.ir.util.ExternalDependenciesGenerator
 import org.jetbrains.kotlin.ir.util.IrMessageLogger
 import org.jetbrains.kotlin.ir.util.ReferenceSymbolTable
 import org.jetbrains.kotlin.ir.util.SymbolTable
 import org.jetbrains.kotlin.ir.util.TypeTranslator
 import org.jetbrains.kotlin.ir.util.dump
+import org.jetbrains.kotlin.ir.visitors.acceptVoid
+import org.jetbrains.kotlin.js.config.ErrorTolerancePolicy
+import org.jetbrains.kotlin.js.config.JSConfigurationKeys
+import org.jetbrains.kotlin.konan.util.KlibMetadataFactories
+import org.jetbrains.kotlin.library.KotlinLibrary
+import org.jetbrains.kotlin.library.impl.isKotlinLibrary
+import org.jetbrains.kotlin.library.resolver.KotlinLibraryResolveResult
+import org.jetbrains.kotlin.library.resolver.TopologicalLibraryOrder
 import org.jetbrains.kotlin.load.kotlin.JvmPackagePartSource
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi2ir.Psi2IrConfiguration
 import org.jetbrains.kotlin.psi2ir.Psi2IrTranslator
 import org.jetbrains.kotlin.psi2ir.generators.DeclarationStubGeneratorImpl
 import org.jetbrains.kotlin.psi2ir.generators.GeneratorContext
+import org.jetbrains.kotlin.psi2ir.generators.GeneratorExtensions
 import org.jetbrains.kotlin.resolve.AnalyzingUtils
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedContainerSource
+import org.jetbrains.kotlin.storage.LockBasedStorageManager
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import java.io.File
 
@@ -488,6 +521,176 @@ abstract class AbstractIrTransformTest : AbstractCodegenTest() {
             irLinker.postProcess()
             return irModuleFragment
         }
+    }
+
+    inner class JsCompilation(private val verifySignatures: Boolean = true) : Compilation {
+        private val classpath = System.getProperty("androidx.compose.js.classpath").orEmpty()
+            .split(":")
+            .map { File(it) }
+            .filter { isKotlinLibrary(it) }
+
+        override val enabled: Boolean = classpath.isNotEmpty()
+
+        override fun compile(files: List<KtFile>): IrModuleFragment {
+            val configuration = newConfiguration()
+            val dependencyFiles = classpath.map { it.absolutePath }
+            configuration.put(JSConfigurationKeys.LIBRARIES, dependencyFiles)
+
+            val environment = KotlinCoreEnvironment.createForTests(
+                myTestRootDisposable,
+                configuration,
+                EnvironmentConfigFiles.JS_CONFIG_FILES
+            )
+            setupEnvironment(environment)
+
+            val analyzer = AnalyzerWithCompilerReport(environment.configuration)
+            val deps = jsResolveLibraries(
+                dependencyFiles,
+                emptyList(),
+                messageCollectorLogger(
+                    environment.configuration[CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY]!! as MessageCollector
+                )
+            )
+
+            val moduleProvider = JsModuleProvider(environment, deps)
+
+            analyzer.analyzeAndReport(files) {
+                TopDownAnalyzerFacadeForJSIR.analyzeFiles(
+                    files,
+                    environment.project,
+                    environment.configuration,
+                    deps.getFullList().map { moduleProvider.getModuleDescriptor(it) },
+                    friendModuleDescriptors = emptyList(),
+                    thisIsBuiltInsModule = false,
+                    customBuiltInsModule = moduleProvider.builtInsModule,
+                    targetEnvironment = org.jetbrains.kotlin.resolve.CompilerEnvironment
+                )
+            }
+
+            val result = analyzer.analysisResult
+            TopDownAnalyzerFacadeForJSIR.checkForErrors(
+                files,
+                result.bindingContext,
+                ErrorTolerancePolicy.NONE
+            )
+            val mangler = JsManglerDesc
+            val signaturer = IdSignatureDescriptor(mangler)
+
+            val psi2Ir = Psi2IrTranslator(
+                configuration.languageVersionSettings,
+                Psi2IrConfiguration(),
+            )
+
+            val symbolTable = SymbolTable(signaturer, PersistentIrFactory())
+
+            val generatorContext = psi2Ir.createGeneratorContext(
+                result.moduleDescriptor,
+                result.bindingContext,
+                symbolTable,
+                GeneratorExtensions()
+            )
+
+            val irBuiltIns = generatorContext.irBuiltIns
+            val functionFactory = IrFunctionFactory(irBuiltIns, generatorContext.symbolTable)
+            irBuiltIns.functionFactory = functionFactory
+
+            val messageLogger = environment.configuration[IrMessageLogger.IR_MESSAGE_LOGGER]
+                ?: IrMessageLogger.None
+
+            val irLinker = JsIrLinker(
+                generatorContext.moduleDescriptor,
+                messageLogger,
+                generatorContext.irBuiltIns,
+                generatorContext.symbolTable,
+                functionFactory,
+                JsIrLinker.JsFePluginContext(
+                    result.moduleDescriptor,
+                    generatorContext.symbolTable,
+                    generatorContext.typeTranslator,
+                    generatorContext.irBuiltIns
+                ),
+            )
+
+            deps.getFullList(TopologicalLibraryOrder).forEach {
+                irLinker.deserializeOnlyHeaderModule(moduleProvider.getModuleDescriptor(it), it)
+            }
+
+            val sybmols = BuiltinSymbolsBase(
+                irBuiltIns,
+                irBuiltIns.builtIns,
+                generatorContext.symbolTable
+            )
+
+            psi2Ir.addPostprocessingStep {
+                postProcessingStep(it, generatorContext, irLinker, messageLogger, sybmols)
+            }
+
+            val moduleFragment = psi2Ir.generateModuleFragment(
+                generatorContext,
+                files,
+                listOf(irLinker),
+                emptyList(),
+            )
+
+            if (verifySignatures) {
+                moduleFragment.acceptVoid(
+                    ManglerChecker(JsManglerIr, Ir2DescriptorManglerAdapter(JsManglerDesc))
+                )
+            }
+
+            irLinker.postProcess()
+
+            return moduleFragment
+        }
+    }
+
+    private class JsModuleProvider(
+        private val environment: KotlinCoreEnvironment,
+        private val deps: KotlinLibraryResolveResult
+    ) {
+        private val JsFactories = KlibMetadataFactories(
+            { object : KotlinBuiltIns(it) {} },
+            DynamicTypeDeserializer
+        )
+
+        var builtInsModule: ModuleDescriptorImpl? = null
+        val descriptors = mutableMapOf<KotlinLibrary, ModuleDescriptorImpl>()
+        private val storageManager: LockBasedStorageManager =
+            LockBasedStorageManager("ModulesStructure")
+
+        private val moduleDependencies: Map<KotlinLibrary, List<KotlinLibrary>> = run {
+            val result = mutableMapOf<KotlinLibrary, List<KotlinLibrary>>()
+
+            deps.forEach { klib, _ ->
+                val dependencies = deps.filterRoots { it.library == klib }.getFullList(
+                    TopologicalLibraryOrder
+                )
+                result[klib] = dependencies.minus(klib)
+            }
+            result
+        }
+
+        fun getModuleDescriptor(current: KotlinLibrary): ModuleDescriptorImpl =
+            if (current in descriptors) {
+                descriptors.getValue(current)
+            } else {
+                JsFactories.DefaultDeserializedDescriptorFactory.createDescriptorOptionalBuiltIns(
+                    current,
+                    environment.configuration.languageVersionSettings,
+                    storageManager,
+                    builtInsModule?.builtIns,
+                    packageAccessHandler = null,
+                    lookupTracker = LookupTracker.DO_NOTHING
+                ).also { md ->
+                    descriptors[current] = md
+                    if (current.isBuiltIns) builtInsModule = md
+                    md.setDependencies(
+                        listOf(md) + moduleDependencies.getValue(current).map {
+                            getModuleDescriptor(it)
+                        }
+                    )
+                }
+            }
     }
 
     // This interface enables different Compilation variants for compiler tests