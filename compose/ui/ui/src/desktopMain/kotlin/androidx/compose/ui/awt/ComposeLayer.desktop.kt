7b49bb8368fff45bc3d4f16e711b463762217980
Aleksandr Veselov <aleksandr.veselov@jetbrains.com>
2022-02-01 12:29:47 +0300

    Enable a11y for Windows, add property to disable a11y (#173)
    # Conflicts:
    #	compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/ComposeScene.desktop.kt
    #	compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/awt/ComposeLayer.desktop.kt

--- compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/awt/ComposeLayer.desktop.kt
+++ compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/awt/ComposeLayer.desktop.kt
@@ -102,10 +102,14 @@ internal class ComposeLayer {
 
     private val density get() = _component.density.density
 
+    private val a11yDisabled by lazy {
+        System.getProperty("compose.accessibility.enable") == "false" ||
+        System.getenv("COMPOSE_DISABLE_ACCESSIBILITY") != null
+    }
+
     fun makeAccessible(component: Component) = object : Accessible {
         override fun getAccessibleContext(): AccessibleContext? {
-            // TODO move System.getenv out of this method
-            if (System.getenv("COMPOSE_DISABLE_ACCESSIBILITY") != null) return null
+            if (a11yDisabled) return null
             val controller =
                 scene.mainOwner?.accessibilityController as? AccessibilityControllerImpl
             val accessible = controller?.rootAccessible
