0bb03e72b154ace35d7ce18407c64707bfe510d5
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2022-06-07 11:36:19 +0200

    fix pointerHoverIcon: update icon when it's changed conditionally (#231)
    * fix pointerHoverIcon: update icon when it's changed conditionally
    
    https://github.com/JetBrains/compose-jb/issues/2091
    
    * add tests
    
    * add DisposableEffect conditionally - only when a component is hovered
    
    Co-authored-by: Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/SkiaBasedOwner.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/SkiaBasedOwner.skiko.kt
@@ -254,7 +254,7 @@ internal class SkiaBasedOwner(
         if (
             measureAndLayoutDelegate.measureAndLayout {
                 if (sendPointerUpdate) {
-                    pointerPositionUpdater.onLayout()
+                    pointerPositionUpdater.needUpdate()
                 }
             }
         ) {
@@ -399,5 +399,9 @@ internal class SkiaBasedOwner(
 
     override val pointerIconService = object : PointerIconService {
         override var current: PointerIcon = PointerIconDefaults.Default
+
+        override fun requestUpdate() {
+            pointerPositionUpdater.needUpdate()
+        }
     }
 }
