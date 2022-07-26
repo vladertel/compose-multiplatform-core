c7e9a0ff91d692808a75c2610c93ab6f5b80f29e
Igor Demin <igordmn@users.noreply.github.com>
2022-07-26 12:41:12 +0200

    ui-test [refactoring] Implement test input methods for desktop (#260)
    * Implement test input methods for desktop
    
    Fixes https://github.com/JetBrains/compose-jb/issues/1216
    Fixes https://github.com/JetBrains/compose-jb/issues/1846
    Fixes https://github.com/JetBrains/compose-jb/issues/2059
    Fixes https://github.com/JetBrains/compose-jb/issues/1127
    
    Partially fixes https://github.com/JetBrains/compose-jb/issues/1177
    
    API changes:
    - make PointerButton non-experimental
    - add a new non-experimental constructor for ComposeScene
    - TestPointerInputEventData is deprecated
    
    During upstream, if new public API will be changed, we have to maintain its backward compatability.
    
    * replace performTouchInput to performMouseInput in tests

--- compose/ui/ui-test/src/commonMain/kotlin/androidx/compose/ui/test/InputDispatcher.kt
+++ compose/ui/ui-test/src/commonMain/kotlin/androidx/compose/ui/test/InputDispatcher.kt
@@ -69,7 +69,9 @@ internal expect fun createInputDispatcher(
 @OptIn(InternalComposeApi::class)
 internal abstract class InputDispatcher(
     private val testContext: TestContext,
-    private val root: RootForTest
+    private val root: RootForTest,
+    private val exitHoverOnPress: Boolean = true,
+    private val moveOnScroll: Boolean = true
 ) {
     companion object {
         /**
@@ -419,7 +421,7 @@ internal abstract class InputDispatcher(
         mouse.setButtonBit(buttonId)
 
         // Exit hovering if necessary
-        if (mouse.isEntered) {
+        if (mouse.isEntered && exitHoverOnPress) {
             mouse.exitHover()
         }
         // down/move + press
@@ -487,7 +489,7 @@ internal abstract class InputDispatcher(
         mouse.enqueueRelease(buttonId)
 
         // When no buttons remaining, enter hover state immediately
-        if (mouse.hasNoButtonsPressed && isWithinRootBounds(currentMousePosition)) {
+        if (exitHoverOnPress && mouse.hasNoButtonsPressed && isWithinRootBounds(currentMousePosition)) {
             mouse.enterHover()
             mouse.enqueueMove()
         }
@@ -554,8 +556,10 @@ internal abstract class InputDispatcher(
     fun enqueueMouseScroll(delta: Float, scrollWheel: ScrollWheel) {
         val mouse = mouseInputState
 
-        // A scroll is always preceded by a move(/hover) event
-        enqueueMouseMove(currentMousePosition)
+        if (moveOnScroll) {
+            // On Android a scroll is always preceded by a move(/hover) event
+            enqueueMouseMove(currentMousePosition)
+        }
         if (isWithinRootBounds(currentMousePosition)) {
             mouse.enqueueScroll(delta, scrollWheel)
         }
