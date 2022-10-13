f166d2a41bf0250df6a4c014bd84d017b04e8ef6
Ember Rose <emberrose@google.com>
2022-10-13 16:15:31 -0400

    Move Paparazzi wrapper to testutils
    Test: ./gradlew :internal-testutils-paparazzi:test
    Fix: 253492148
    Change-Id: I67cc15f38e58a0bd59b7a2852e7a99ac5aa5cea6
    
    
    GitOrigin-RevId: 07c3a14b8d488e44dea5d959469b168ff1fa7835

---
+++ compose/testutils/testutils-paparazzi/src/main/kotlin/androidx/testutils/paparazzi/AndroidXPaparazzi.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.testutils.paparazzi
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Environment
+import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams.RenderingMode
+import java.io.File
+
+/**
+ * Creates a [Paparazzi] test rule configured from system properties for AndroidX tests.
+ */
+fun androidxPaparazzi(
+    deviceConfig: DeviceConfig = DeviceConfig.PIXEL_6.copy(softButtons = false),
+    theme: String = "android:Theme.Material.NoActionBar.Fullscreen",
+    renderingMode: RenderingMode = RenderingMode.SHRINK,
+    imageDiffer: ImageDiffer = ImageDiffer.PixelPerfect
+) = Paparazzi(
+    deviceConfig = deviceConfig,
+    theme = theme,
+    renderingMode = renderingMode,
+    environment = Environment(
+        platformDir = systemProperty("platformDir"),
+        resDir = systemProperty("resDir"),
+        assetsDir = systemProperty("assetsDir"),
+        compileSdkVersion = systemProperty("compileSdkVersion").toInt(),
+        resourcePackageNames = systemProperty("resourcePackageNames").split(","),
+        appTestDir = System.getProperty("user.dir")!!
+    ),
+    snapshotHandler = GoldenVerifier(
+        modulePath = systemProperty("modulePath"),
+        goldenRootDirectory = File(systemProperty("goldenRootDir")),
+        reportDirectory = File(systemProperty("reportDir")),
+        imageDiffer = imageDiffer
+    )
+)
+
+/** Package name used for resolving system properties */
+private const val PACKAGE_NAME = "androidx.testutils.paparazzi"
+
+/** Read a system property with [PACKAGE_NAME] prefix, throwing an exception if missing */
+private fun systemProperty(name: String) =
+    requireNotNull(System.getProperty("$PACKAGE_NAME.$name")) {
+        "System property $PACKAGE_NAME.$name is not set. You may need to apply " +
+            "AndroidXPaparazziPlugin to your Gradle build."
+    }
