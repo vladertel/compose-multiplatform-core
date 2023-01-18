bb9b608fa194fb275505369d5ccfd0340608bee4
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-01-18 11:16:16 +0100

    Update compilerVersion in VersionChecker (1.4.0-rc01)

--- compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -116,7 +116,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.4.0"
+        const val compilerVersion: String = "1.4.0-rc01"
         private val minimumRuntimeVersion: String
             get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
