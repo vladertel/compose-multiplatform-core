46b87aff21ddcacfc4e4dea96699e26f3e25d2f6
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2023-03-29 17:32:44 +0200

    Fix compilation errors for compileKotlinJs task

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/DeadKeyCombiner.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/DeadKeyCombiner.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import androidx.compose.ui.input.key.KeyEvent
  *
  * It is NOT thread safe.
  */
-internal expect class DeadKeyCombiner {
+internal expect class DeadKeyCombiner() {
 
     /**
      * @param event the key event received by the combiner
--- compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/text/DeadKeyCombiner.skiko.kt
+++ compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/text/DeadKeyCombiner.skiko.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,5 +21,6 @@ import androidx.compose.ui.input.key.utf16CodePoint
 
 internal actual class DeadKeyCombiner {
     // TODO needs actual impl
+    // TODO: [1.4 Update] provide proper implementation
     actual fun consume(event: KeyEvent): Int? = event.utf16CodePoint
 }
