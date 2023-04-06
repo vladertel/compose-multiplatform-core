30fbfe66a675b6e2efe697b3a4d6f4cbc6e96c25
oleksandr.karpovich <a.n.karpovich@gmail.com>
2023-04-06 16:43:26 +0200

    Update compose compiler plugin for kotlin 1.9.20-dev-5418

--- compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
+++ compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
@@ -3963,7 +3963,7 @@ class ComposableFunctionBodyTransformer(
                 var parent = function.parent
                 while (true) {
                     when (parent) {
-                        is IrPackageFragment -> return parent.fqName.asString()
+                        is IrPackageFragment -> return parent.packageFqName.asString()
                         is IrDeclaration -> parent = parent.parent
                         else -> break
                     }
