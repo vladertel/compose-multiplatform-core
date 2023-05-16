e7e8c4530df466c0584ea18751a71e1a82d3face
Alexander Maryanovsky <alexander.maryanovsky@jetbrains.com>
2023-05-16 16:45:56 +0300

    Reduce the number of threads to 20.

--- compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/ImageComposeSceneTest.kt
+++ compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/ImageComposeSceneTest.kt
@@ -101,7 +101,7 @@ class ImageComposeSceneTest {
     @OptIn(ExperimentalTime::class)
     @Test(timeout = 5000)
     fun `run multiple ImageComposeScenes concurrently`() {
-        val service = Executors.newFixedThreadPool(39)
+        val service = Executors.newFixedThreadPool(20)
 
         for(i in 1..1000) {
             service.submit {
