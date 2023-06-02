be811bb069dd7c2161dad2e2823c9ff7821fc0ee
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-06-02 11:41:41 +0200

    Add multiple ComposeWindow <-> UINavigationController sample

--- compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/main.uikit.kt
+++ compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/main.uikit.kt
@@ -2,9 +2,54 @@
 package androidx.compose.mpp.demo
 
 import ApplicationLayoutExamples
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.LocalUIViewController
 import androidx.compose.ui.main.defaultUIKitMain
 import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UINavigationController
+import platform.UIKit.childViewControllers
+import platform.UIKit.navigationController
+
+@Composable
+fun NativePopupPresenter() {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        val viewController = LocalUIViewController.current
+        Button(onClick = {
+            val navigationController = UINavigationController(rootViewController = ComposeUIViewController {
+                NativePopupNavigationPage()
+            })
+
+            viewController.presentViewController(navigationController, true, null)
+        }) {
+            Text("Present popup")
+        }
+    }
+}
+
+@Composable
+fun NativePopupNavigationPage() {
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        val navigationController = LocalUIViewController.current.navigationController
+
+        Button(onClick = {
+            navigationController?.pushViewController(
+                ComposeUIViewController {
+                    NativePopupNavigationPage()
+                }, true
+            )
+        }) {
+            Text("Push")
+        }
+    }
+}
 
 fun main() {
     defaultUIKitMain("ComposeDemo", ComposeUIViewController {
@@ -15,12 +60,16 @@ fun main() {
 @Composable
 fun IosDemo() {
     // You may uncomment different examples:
-//    MultiplatformDemo()
-    ApplicationLayoutExamples()
+    MultiplatformDemo()
+//    ApplicationLayoutExamples()
 }
 
 @Composable
 fun MultiplatformDemo() {
-    val app = remember { App() }
+    val app = remember {
+        App(extraScreens = listOf(Screen.Example("Native popup presenter") {
+            NativePopupPresenter()
+        }))
+    }
     app.Content()
 }
