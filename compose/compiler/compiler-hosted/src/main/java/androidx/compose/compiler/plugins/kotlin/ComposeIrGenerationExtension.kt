6e030313c758c2fd3ff333ac773b0973efe408c2
oleksandr.karpovich <a.n.karpovich@gmail.com>
2023-07-26 12:57:05 +0200

    add k/wasm checks in compose compiler plugin

--- compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposeIrGenerationExtension.kt
+++ compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposeIrGenerationExtension.kt
@@ -49,6 +49,7 @@ import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsManglerIr
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.visitors.acceptVoid
 import org.jetbrains.kotlin.platform.isJs
+import org.jetbrains.kotlin.platform.isWasm
 import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.platform.konan.isNative
 
@@ -165,11 +166,16 @@ class ComposeIrGenerationExtension(
 
         val mangler = when {
             pluginContext.platform.isJs() -> JsManglerIr
+            pluginContext.platform.isWasm() -> JsManglerIr
             else -> null
         }
 
         val idSignatureBuilder = when {
             pluginContext.platform.isJs() -> IdSignatureSerializer(
+                PublicIdSignatureComputer(mangler!!),
+                DeclarationTable(JsGlobalDeclarationTable(pluginContext.irBuiltIns))
+            )
+            pluginContext.platform.isWasm() -> IdSignatureSerializer(
                 PublicIdSignatureComputer(mangler!!),
                 DeclarationTable(JsGlobalDeclarationTable(pluginContext.irBuiltIns))
             )
@@ -255,7 +261,7 @@ class ComposeIrGenerationExtension(
             ).lower(moduleFragment)
         }
 
-        if (pluginContext.platform.isJs()) {
+        if (pluginContext.platform.isWasm() || pluginContext.platform.isJs()) {
             WrapJsComposableLambdaLowering(
                 pluginContext,
                 symbolRemapper,
