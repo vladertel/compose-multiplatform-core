feaedf1dda87ba0b962856c34daf4c0f5f25ada9
oleksandr.karpovich <a.n.karpovich@gmail.com>
2023-08-16 16:08:19 +0200

    Temporary revert change in ComposableFunInterfaceLowering (28b3ce9ff619547887af6814874d433578f38ba5)
    It crashes in some cases at runtime. Reported to JC team. Waiting for a proper fix.

--- compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunInterfaceLowering.kt
+++ compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunInterfaceLowering.kt
@@ -23,6 +23,7 @@ import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrFunctionExpression
+import org.jetbrains.kotlin.ir.expressions.IrTypeOperator
 import org.jetbrains.kotlin.ir.expressions.IrTypeOperator.IMPLICIT_CAST
 import org.jetbrains.kotlin.ir.expressions.IrTypeOperator.SAM_CONVERSION
 import org.jetbrains.kotlin.ir.expressions.IrTypeOperatorCall
@@ -45,10 +46,51 @@ class ComposableFunInterfaceLowering(private val context: IrPluginContext) :
         }
     }
 
+//    override fun visitTypeOperator(expression: IrTypeOperatorCall): IrExpression {
+//        val functionExpr = expression.findSamFunctionExpr()
+//        if (functionExpr != null && expression.typeOperand.isComposableFunInterface()) {
+//            val argument = functionExpr.transform(this, null) as IrFunctionExpression
+//            val superType = expression.typeOperand
+//            val superClass = superType.classOrNull ?: error("Expected non-null class")
+//            return FunctionReferenceBuilder(
+//                argument,
+//                superClass,
+//                superType,
+//                currentDeclarationParent!!,
+//                context,
+//                currentScope!!.scope.scopeOwnerSymbol,
+//                IrTypeSystemContextImpl(context.irBuiltIns)
+//            ).build()
+//        }
+//        return super.visitTypeOperator(expression)
+//    }
+
+    private fun isFunInterfaceConversion(expression: IrTypeOperatorCall): Boolean {
+        val argument = expression.argument
+        val operator = expression.operator
+        val type = expression.typeOperand
+        val functionClass = type.classOrNull
+        return operator == IrTypeOperator.SAM_CONVERSION &&
+            argument is IrFunctionExpression &&
+            argument.origin.isLambda &&
+            functionClass != null &&
+            functionClass.owner.isFun
+        // IMPORTANT(b/178663739):
+        // We are transforming not just SAM conversions for composable fun interfaces, but ALL
+        // fun interfaces temporarily until KT-44622 gets fixed in the version of kotlin we
+        // are using, which should be in 1.4.30.
+        // Once it does, we should either add the below additional condition to this predicate,
+        // or, if possible, remove this lowering all together if kotlin's lowering works for
+        // composable fun interfaces as well.
+        //
+        // functionClass.functions.single {
+        //    it.owner.modality == Modality.ABSTRACT
+        // }.owner.annotations.hasAnnotation(ComposeFqNames.Composable)
+    }
+
     override fun visitTypeOperator(expression: IrTypeOperatorCall): IrExpression {
-        val functionExpr = expression.findSamFunctionExpr()
-        if (functionExpr != null && expression.typeOperand.isComposableFunInterface()) {
-            val argument = functionExpr.transform(this, null) as IrFunctionExpression
+        if (isFunInterfaceConversion(expression)) {
+            val argument = expression.argument.transform(this, null) as IrFunctionExpression
             val superType = expression.typeOperand
             val superClass = superType.classOrNull ?: error("Expected non-null class")
             return FunctionReferenceBuilder(
