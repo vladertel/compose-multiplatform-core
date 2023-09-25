6c1619cbafa9703cd86ca5db5768ef7cc426deb9
oleksandr.karpovich <a.n.karpovich@gmail.com>
2023-09-25 11:17:38 +0200

    upd to kotlin 1.9.20-Beta2

--- buildSrc/private/src/main/kotlin/androidx/build/AndroidXComposeImplPlugin.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/AndroidXComposeImplPlugin.kt
@@ -366,7 +366,7 @@ private fun configureComposeCompilerPlugin(
         val configuration = project.configurations.create(COMPILER_PLUGIN_CONFIGURATION)
         // Add Compose compiler plugin to kotlinPlugin configuration, making sure it works
         // for Playground builds as well
-        project.dependencies.add(COMPILER_PLUGIN_CONFIGURATION, "org.jetbrains.compose.compiler:compiler:0.0.0-1.9.20-Beta-205")
+        project.dependencies.add(COMPILER_PLUGIN_CONFIGURATION, "org.jetbrains.compose.compiler:compiler:1.5.2.1-Beta3")
 //        project.dependencies.add(COMPILER_PLUGIN_CONFIGURATION, "org.jetbrains.compose.compiler:compiler:1.5.1")
 //        project.dependencies.add(
 //            COMPILER_PLUGIN_CONFIGURATION,
