5bc13d10f489ad5ec39973bdb8022c5ecd72d3c4
oleksandr.karpovich <a.n.karpovich@gmail.com>
2023-12-04 17:54:20 +0100

    remove wasm/experimental maven repositories

--- buildSrc/plugins/src/main/kotlin/androidx/build/AndroidXPlugin.kt
+++ buildSrc/plugins/src/main/kotlin/androidx/build/AndroidXPlugin.kt
@@ -35,19 +35,6 @@ class AndroidXPlugin : Plugin<Project> {
                 "from" to "$supportRoot/buildSrc/apply/applyAndroidXImplPlugin.gradle"
             )
         )
-
-        project.configurations.all {
-            // TODO: remove these HACKS when possible
-            val conf = it
-            conf.resolutionStrategy.eachDependency {
-                val isWasm = conf.name.contains("wasm", true)
-                if (it.requested.module.group == "org.jetbrains.kotlinx" &&
-                    it.requested.module.name.contains("kotlinx-coroutines", true)
-                ) {
-                    if (!isWasm) it.useVersion("1.7.2")
-                }
-            }
-        }
     }
 
     companion object {
