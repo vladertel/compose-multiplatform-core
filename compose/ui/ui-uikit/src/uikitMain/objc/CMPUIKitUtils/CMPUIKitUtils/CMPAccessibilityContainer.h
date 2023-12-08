39493372bcb8aff30029846adb672b07eff4ce4a
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-12-08 12:38:39 +0100

    Update licenses

--- compose/ui/ui-uikit/src/uikitMain/objc/CMPUIKitUtils/CMPUIKitUtils/CMPAccessibilityContainer.h
+++ compose/ui/ui-uikit/src/uikitMain/objc/CMPUIKitUtils/CMPUIKitUtils/CMPAccessibilityContainer.h
@@ -1,9 +1,18 @@
-//
-//  CMPAccessibilityContainer.h
-//  CMPUIKitUtils
-//
-//  Created by Ilia.Semenov on 06/12/2023.
-//
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import <UIKit/UIKit.h>
 
@@ -11,7 +20,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CMPAccessibilityContainer : UIAccessibilityElement
 
-// need to be redeclared here because of the following issue
+// Redeclared to make it visible to Kotlin, workaround for the following issue:
 // https://youtrack.jetbrains.com/issue/KT-56001/Kotlin-Native-import-Objective-C-category-members-as-class-members-if-the-category-is-located-in-the-same-file
 
 - (__nullable id)accessibilityElementAtIndex:(NSInteger)index;
