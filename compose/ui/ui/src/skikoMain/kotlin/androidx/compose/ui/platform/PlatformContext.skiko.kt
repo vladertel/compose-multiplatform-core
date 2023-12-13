3ae1949865002ab7e6adda0ed8fc24693a06b25c
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-12-13 10:06:56 +0100

    Add todo

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/PlatformContext.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/PlatformContext.skiko.kt
@@ -87,6 +87,7 @@ interface PlatformContext {
         fun onRootForTestDisposed(root: PlatformRootForTest)
     }
 
+    // TODO: discuss whether this interface should be nested in PlatformContext
     interface SemanticsOwnerListener {
         /**
          * Callback method that is called when a [SemanticsOwner] is appended to tracking.
