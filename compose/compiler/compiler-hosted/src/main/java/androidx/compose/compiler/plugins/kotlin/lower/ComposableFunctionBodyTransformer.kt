7fa13f4c1782cc559a540756c609620af000e5bb
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-01-02 14:14:52 +0100

    Handle DecoyFqNames.remember calls in ComposableFunctionBodyTransformer

--- compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
+++ compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
@@ -2927,7 +2927,8 @@ class ComposableFunctionBodyTransformer(
 
     private fun visitComposableCall(expression: IrCall): IrExpression {
         return when (expression.symbol.owner.kotlinFqName) {
-            ComposeFqNames.remember -> {
+            ComposeFqNames.remember,
+            DecoyFqNames.remember -> {
                 if (intrinsicRememberEnabled) {
                     visitRememberCall(expression)
                 } else {
