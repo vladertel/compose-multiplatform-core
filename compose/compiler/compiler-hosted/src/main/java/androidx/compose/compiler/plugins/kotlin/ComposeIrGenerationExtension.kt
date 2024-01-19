bda1893515870cadee87eaf72ac6911a32a09404
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-01-19 10:34:37 +0100

    Add StabilityInferred annotation to the classes descriptors during the serialization

--- compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposeIrGenerationExtension.kt
+++ compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposeIrGenerationExtension.kt
@@ -19,6 +19,7 @@ package androidx.compose.compiler.plugins.kotlin
 import androidx.compose.compiler.plugins.kotlin.analysis.FqNameMatcher
 import androidx.compose.compiler.plugins.kotlin.analysis.StabilityInferencer
 import androidx.compose.compiler.plugins.kotlin.lower.ClassStabilityTransformer
+import androidx.compose.compiler.plugins.kotlin.lower.ClassStabilityInferredCollection
 import androidx.compose.compiler.plugins.kotlin.lower.ComposableFunInterfaceLowering
 import androidx.compose.compiler.plugins.kotlin.lower.ComposableFunctionBodyTransformer
 import androidx.compose.compiler.plugins.kotlin.lower.ComposableLambdaAnnotator
@@ -68,6 +69,7 @@ class ComposeIrGenerationExtension(
     private val stableTypeMatchers: Set<FqNameMatcher> = emptySet(),
     private val moduleMetricsFactory: ((StabilityInferencer) -> ModuleMetrics)? = null,
     private val hideFromObjCDeclarationsSet: HideFromObjCDeclarationsSet? = null,
+    private val classStabilityInferredCollection: ClassStabilityInferredCollection? = null
 ) : IrGenerationExtension {
     var metrics: ModuleMetrics = EmptyModuleMetrics
         private set
@@ -119,7 +121,10 @@ class ComposeIrGenerationExtension(
             pluginContext,
             symbolRemapper,
             metrics,
-            stabilityInferencer
+            stabilityInferencer,
+            classStabilityInferredCollection = classStabilityInferredCollection?.takeIf {
+                pluginContext.platform?.isJvm() == false
+            }
         ).lower(moduleFragment)
 
         LiveLiteralTransformer(
