76c7b9da160843a4e1621ef683386a3a264c9e7e
dima.avdeev <dima.avdeev@jetbrains.com>
2024-01-31 16:40:07 +0400

    minify sample

--- compose/mpp/demo/src/uikitMain/kotlin/bugs/TextFieldCrashVisualTransformation.kt
+++ compose/mpp/demo/src/uikitMain/kotlin/bugs/TextFieldCrashVisualTransformation.kt
@@ -18,6 +18,7 @@ package bugs
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -25,8 +26,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.mandatorySystemGestures
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.mpp.demo.Screen
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,6 +38,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
@@ -52,27 +58,21 @@ import platform.Foundation.NSNumberFormatterCurrencyStyle
 val TextFieldCrashVisualTransformation = Screen.Example(
     "TextFieldCrashVisualTransformation"
 ) {
-    var value by remember { mutableStateOf("") }
     val vs = rememberStyleNumberVisualTransformation("USD")
+    val focusRequester = remember { FocusRequester() }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .windowInsetsPadding(WindowInsets.mandatorySystemGestures)
-    ) {
-        BasicTextField(
-            value = value,
-            onValueChange = {
-                value = it.filter(Char::isDigit)
-            },
+    Column {
+        Text("Click to TextField bellow to reproduce crash")
+        OutlinedTextField(
+            value = "1000",
+            onValueChange = {},
             visualTransformation = vs,
-            textStyle = TextStyle(fontSize = 20.sp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .align(Alignment.Center)
-                .background(Color.Red)
+            modifier = Modifier.fillMaxWidth().focusRequester(focusRequester)
         )
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
     }
 }
 
