d5300b2dccae1082d4626f7c0089a1994366228d
Pavel Shishkin <pavel.shishkin@jetbrains.com>
2024-03-12 19:57:23 +0100

    update kotlin version to 2.0.0-dev-18368 (pre-Beta5)

--- compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ compose/compiler/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -143,7 +143,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.5.9-dev1-kt2.0.0-Beta3"
+        const val compilerVersion: String = "1.5.10.1-dev1-kt2.0.0-dev-18368"
         private val minimumRuntimeVersion: String
             get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
