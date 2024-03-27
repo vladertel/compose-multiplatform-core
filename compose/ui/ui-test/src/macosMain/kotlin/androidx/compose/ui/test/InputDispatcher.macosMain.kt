007f80b2977a9668b98751302dc193a243f4b5bb
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2024-03-27 16:45:13 +0100

    Fix InputDispatcher.macosMain.kt

--- compose/ui/ui-test/src/macosMain/kotlin/androidx/compose/ui/test/InputDispatcher.macosMain.kt
+++ compose/ui/ui-test/src/macosMain/kotlin/androidx/compose/ui/test/InputDispatcher.macosMain.kt
@@ -16,13 +16,12 @@
 package androidx.compose.ui.test
 
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.key.InputModifiers
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.NativeKeyEvent
 import org.jetbrains.skiko.SkikoInputModifiers
-import org.jetbrains.skiko.SkikoKey
-import org.jetbrains.skiko.SkikoKeyboardEvent
-import org.jetbrains.skiko.SkikoKeyboardEventKind
 
 /**
  * The [KeyEvent] is usually created by the system. This function creates an instance of
@@ -34,17 +33,10 @@ internal actual fun keyEvent(
     modifiers: Int
 ): KeyEvent {
     return KeyEvent(
-        SkikoKeyboardEvent(
-            key = SkikoKey.values().firstOrNull {
-                it.platformKeyCode.toLong() == key.keyCode
-            } ?: error("SkikoKey not found for key=$key"),
-            modifiers = SkikoInputModifiers(modifiers),
-            kind = when (keyEventType) {
-                KeyEventType.KeyUp -> SkikoKeyboardEventKind.UP
-                KeyEventType.KeyDown -> SkikoKeyboardEventKind.DOWN
-                else -> error("Unknown key event type: $keyEventType")
-
-            },
+        NativeKeyEvent(
+            key = key,
+            modifiers = InputModifiers(modifiers),
+            kind = keyEventType,
             timestamp = 0L,
             platform = null
         )
