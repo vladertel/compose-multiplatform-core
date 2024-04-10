b6b046a6898ba74ff1894d641861b9572878d8ae
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2024-04-10 13:44:17 +0200

    Update TODO

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/Accessibility.uikit.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/Accessibility.uikit.kt
@@ -603,7 +603,7 @@ private class AccessibilityElement(
             if (config.contains(SemanticsProperties.LiveRegion)) {
                 // TODO: LiveRegionMode in the config is currently ignored.
                 //  the default behavior due this flag set will actually do `Polite` announcements
-                //  to do `Assertive` announcements, we need to post a notification manually on each change
+                //  to do `Assertive` announcements, we need to post a notification explicitly on each change
                 //  which we need to track manually
                 result = result or UIAccessibilityTraitUpdatesFrequently
             }
