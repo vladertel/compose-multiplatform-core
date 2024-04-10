a8485c7922a406b72a54bd835ea4e23941bcb78d
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2024-04-10 10:25:15 +0200

    Introduce separate SynchronizedTextArea - it's easier to test code when it isolated this way

--- compose/ui/ui/src/webCommonW3C/kotlin/androidx/compose/ui/platform/WebImeInputService.kt
+++ compose/ui/ui/src/webCommonW3C/kotlin/androidx/compose/ui/platform/WebImeInputService.kt
@@ -31,8 +31,13 @@ import kotlinx.browser.document
 import org.w3c.dom.HTMLElement
 import org.w3c.dom.HTMLTextAreaElement
 
-internal class WebImeInputService(parentInputService: InputAwareInputService) : PlatformTextInputService, InputAwareInputService by parentInputService{
-    private var currentDomInput: HTMLTextAreaElement? = null
+internal class WebImeInputService(parentInputService: InputAwareInputService) : PlatformTextInputService, InputAwareInputService by parentInputService {
+
+    private var synchronizedTextArea: SynchronizedTextArea? = null
+        set(value) {
+            field?.dispose()
+            field = value
+        }
 
     override fun startInput(
         value: TextFieldValue,
@@ -40,131 +45,31 @@ internal class WebImeInputService(parentInputService: InputAwareInputService) : 
         onEditCommand: (List<EditCommand>) -> Unit,
         onImeActionPerformed: (ImeAction) -> Unit
     ) {
-        val input = createHtmlInput(imeOptions, onEditCommand)
-        document.body?.appendChild(input)
-        currentDomInput = input
+        synchronizedTextArea = SynchronizedTextArea(imeOptions, onEditCommand)
+        synchronizedTextArea?.register()
 
         showSoftwareKeyboard()
     }
 
     override fun stopInput() {
-        currentDomInput?.remove()
+        synchronizedTextArea?.dispose()
     }
 
     override fun showSoftwareKeyboard() {
-        currentDomInput?.focus()
+        synchronizedTextArea?.focus()
     }
 
     override fun hideSoftwareKeyboard() {
-        currentDomInput?.blur()
+        synchronizedTextArea?.blur()
     }
 
     override fun updateState(oldValue: TextFieldValue?, newValue: TextFieldValue) {
-        currentDomInput ?: return
-        currentDomInput?.value = newValue.text
-        currentDomInput?.setSelectionRange(newValue.selection.start, newValue.selection.end)
+        synchronizedTextArea?.updateState(newValue)
     }
 
     override fun notifyFocusedRect(rect: Rect) {
         super.notifyFocusedRect(rect)
-        updateHtmlInputPosition(getOffset(rect))
+        synchronizedTextArea?.updateHtmlInputPosition(getOffset(rect))
     }
 
-    private fun updateHtmlInputPosition(offset: Offset) {
-        currentDomInput?.style?.left = "${offset.x}px"
-        currentDomInput?.style?.top = "${offset.y}px"
-
-        currentDomInput?.focus()
-    }
-
-    private fun createHtmlInput(imeOptions: ImeOptions, onEditCommand: (List<EditCommand>) -> Unit): HTMLTextAreaElement {
-        val htmlInput = document.createElement("textarea") as HTMLTextAreaElement
-
-        htmlInput.setAttribute("autocorrect", "off")
-        htmlInput.setAttribute("autocomplete", "off")
-        htmlInput.setAttribute("autocapitalize", "off")
-        htmlInput.setAttribute("spellcheck", "false")
-
-        val inputMode = when (imeOptions.keyboardType) {
-            KeyboardType.Text -> "text"
-            KeyboardType.Ascii -> "text"
-            KeyboardType.Number -> "number"
-            KeyboardType.Phone -> "tel"
-            KeyboardType.Uri -> "url"
-            KeyboardType.Email -> "email"
-            KeyboardType.Password -> "password"
-            KeyboardType.NumberPassword -> "number"
-            KeyboardType.Decimal -> "decimal"
-            else -> "text"
-        }
-
-        val enterKeyHint = when (imeOptions.imeAction) {
-            ImeAction.Default -> "enter"
-            ImeAction.None -> "enter"
-            ImeAction.Done -> "done"
-            ImeAction.Go -> "go"
-            ImeAction.Next -> "next"
-            ImeAction.Previous -> "previous"
-            ImeAction.Search -> "search"
-            ImeAction.Send -> "send"
-            else -> "enter"
-        }
-
-        htmlInput.setAttribute("inputmode", inputMode)
-        htmlInput.setAttribute("enterkeyhint", enterKeyHint)
-
-        htmlInput.style.apply {
-            setProperty("position", "absolute")
-            setProperty("user-select", "none")
-            setProperty("forced-color-adjust", "none")
-            setProperty("white-space", "pre-wrap")
-            setProperty("align-content", "center")
-            setProperty("top", "0")
-            setProperty("left", "0")
-            setProperty("padding", "0")
-            setProperty("opacity", "0")
-            setProperty("color", "transparent")
-            setProperty("background", "transparent")
-            setProperty("caret-color", "transparent")
-            setProperty("outline", "none")
-            setProperty("border", "none")
-            setProperty("resize", "none")
-            setProperty("text-shadow", "none")
-        }
-
-        htmlInput.addEventListener("input", {
-                val text = htmlInput.value
-                val cursorPosition = htmlInput.selectionEnd
-                sendImeValueToCompose(onEditCommand, text, cursorPosition)
-        })
-
-        return htmlInput
-    }
-
-    private fun sendImeValueToCompose(
-        onEditCommand: (List<EditCommand>) -> Unit,
-        text: String,
-        newCursorPosition: Int? = null
-    ) {
-        val value = if (text == "\n") {
-            ""
-        } else {
-            text
-        }
-
-        if (newCursorPosition != null) {
-            onEditCommand(
-                listOf(
-                    DeleteAllCommand(),
-                    CommitTextCommand(value, newCursorPosition),
-                )
-            )
-        } else {
-            onEditCommand(
-                listOf(
-                    CommitTextCommand(value, 1)
-                )
-            )
-        }
-    }
 }
