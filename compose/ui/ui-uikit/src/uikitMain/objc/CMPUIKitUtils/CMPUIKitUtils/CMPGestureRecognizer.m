8f7fabea9563aef0385ffd86a73a49249984cfab
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2024-07-01 10:29:04 +0200

    Update internal state on gesture recognizer

--- compose/ui/ui-uikit/src/uikitMain/objc/CMPUIKitUtils/CMPUIKitUtils/CMPGestureRecognizer.m
+++ compose/ui/ui-uikit/src/uikitMain/objc/CMPUIKitUtils/CMPUIKitUtils/CMPGestureRecognizer.m
@@ -21,18 +21,49 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self.proxy touchesBegan:touches withEvent:event];
+    
+    if (self.state == UIGestureRecognizerStatePossible) {
+        self.state = UIGestureRecognizerStateBegan;
+    }
 }
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self.proxy touchesMoved:touches withEvent:event];
+    
+    switch (self.state) {
+        case UIGestureRecognizerStateBegan:
+        case UIGestureRecognizerStateChanged:
+            self.state = UIGestureRecognizerStateChanged;
+            break;
+        default:
+            break;
+    }
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self.proxy touchesEnded:touches withEvent:event];
+    
+    switch (self.state) {
+        case UIGestureRecognizerStateBegan:
+        case UIGestureRecognizerStateChanged:
+            self.state = UIGestureRecognizerStateEnded;
+            break;
+        default:
+            break;
+    }
 }
 
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self.proxy touchesCancelled:touches withEvent:event];
+    
+    switch (self.state) {
+        case UIGestureRecognizerStateBegan:
+        case UIGestureRecognizerStateChanged:
+            self.state = UIGestureRecognizerStateCancelled;
+            break;
+        default:
+            break;
+    }
 }
 
 @end
