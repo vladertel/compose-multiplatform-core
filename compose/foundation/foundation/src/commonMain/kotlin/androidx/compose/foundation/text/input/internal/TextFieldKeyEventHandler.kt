be5d432c580b9c8d4200d64ebf89ec4c18519a04
Igor Demin <igordmn@gmail.com>
2024-07-22 13:31:07 +0200

    bug. foundation-text. TextField key consumption

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/input/internal/TextFieldKeyEventHandler.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/input/internal/TextFieldKeyEventHandler.kt
@@ -77,10 +77,6 @@ internal abstract class TextFieldKeyEventHandler {
         singleLine: Boolean,
         onSubmit: () -> Unit
     ): Boolean {
-        if (event.type != KeyEventType.KeyDown) {
-            return false
-        }
-
         if (event.isTypedEvent) {
             val codePoint = deadKeyCombiner.consume(event)
             if (codePoint != null) {
@@ -98,6 +94,10 @@ internal abstract class TextFieldKeyEventHandler {
                     false
                 }
             }
+        }
+
+        if (event.type != KeyEventType.KeyDown) {
+            return false
         }
 
         val command = keyMapping.map(event)
