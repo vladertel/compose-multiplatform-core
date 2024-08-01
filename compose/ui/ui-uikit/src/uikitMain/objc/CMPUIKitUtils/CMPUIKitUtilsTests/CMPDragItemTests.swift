08286c1a62e93c8465bbf8f04df4e1ce7ef92e90
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2024-08-01 09:18:42 +0200

    Add tests

---
+++ compose/ui/ui-uikit/src/uikitMain/objc/CMPUIKitUtils/CMPUIKitUtilsTests/CMPDragItemTests.swift
@@ -0,0 +1,18 @@
+//
+//  CMPDragItemTests.swift
+//  CMPUIKitUtilsTests
+//
+//  Created by Ilia.Semenov on 26/07/2024.
+//
+
+import XCTest
+
+final class CMPDragItemTests: XCTestCase {
+    func testEncodingAndDecoding() {
+        let string = "Test string"
+        
+        let item = UIDragItem.cmp_item(with: string)
+        
+        
+    }
+}
