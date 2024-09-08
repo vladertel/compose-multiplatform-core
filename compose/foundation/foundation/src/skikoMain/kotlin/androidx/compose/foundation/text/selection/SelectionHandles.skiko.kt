44c3752cd9df47e64c8bc71f21cb0bc7e6ba4962
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2024-09-08 16:14:05 +0300

    rollback irrelevant changes

--- compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/text/selection/SelectionHandles.skiko.kt
+++ compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/text/selection/SelectionHandles.skiko.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.text.textFieldPointer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithCache
