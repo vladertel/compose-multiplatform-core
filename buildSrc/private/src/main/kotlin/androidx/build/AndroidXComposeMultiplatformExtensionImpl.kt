2270bb89a7afee7c976e1fd0e88f03ddc8a12e2a
Andrei Salavei <Andrei.Salavei@jetbrains.com>
2024-09-17 15:12:40 +0200

    Remove unnecessary comments in AndroidXComposeMultiplatformExtensionImpl.kt (#1570)

--- buildSrc/private/src/main/kotlin/androidx/build/AndroidXComposeMultiplatformExtensionImpl.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/AndroidXComposeMultiplatformExtensionImpl.kt
@@ -31,7 +31,6 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithSimulatorTests
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
 import org.jetbrains.kotlin.gradle.targets.native.DefaultSimulatorTestRun
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.tomlj.Toml
@@ -259,22 +258,6 @@ open class AndroidXComposeMultiplatformExtensionImpl @Inject constructor(
     // https://youtrack.jetbrains.com/issue/KT-55751/MPP-Gradle-Consumable-configurations-must-have-unique-attributes
     private val instrumentedTestAttribute = Attribute.of("instrumentedTest", String::class.java)
     private val instrumentedTestCompilationAttribute = Attribute.of("instrumentedTestCompilation", String::class.java)
-
-//    The consumer was configured to find a library for use during 'kotlin-metadata',
-//    preferably optimized for non-jvm, as well as
-//    attribute 'org.jetbrains.kotlin.platform.type'
-//        with value 'native',
-//    attribute 'org.jetbrains.kotlin.native.target'
-//        with value 'ios_simulator_arm64',
-//    attribute 'instrumentedTest'
-//        with value 'Test'.
-//    However we cannot choose between the following variants of project :compose:ui:ui:
-//        - uikitInstrumentedSimArm64ApiElements
-//        - uikitInstrumentedSimArm64MetadataElements
-//        - uikitSimArm64ApiElements
-//        - uikitSimArm64MetadataElements
-
-
     override fun iosInstrumentedTest(): Unit =
         multiplatformExtension.run {
             fun getDeviceName(): String? {
