c3b2d248f1e0ec367254fc3143482176895dab2a
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-09-17 16:42:22 +0200

    Add a reproducer for https://youtrack.jetbrains.com/issue/CMP-6681/Swing-interop-issues-on-Windows

--- compose/mpp/demo/src/desktopMain/kotlin/androidx/compose/mpp/demo/Main.desktop.kt
+++ compose/mpp/demo/src/desktopMain/kotlin/androidx/compose/mpp/demo/Main.desktop.kt
@@ -16,38 +16,122 @@
 
 package androidx.compose.mpp.demo
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.ComposePanel
+import androidx.compose.ui.awt.SwingPanel
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalFontFamilyResolver
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
+import java.awt.BorderLayout
+import java.awt.Dimension
+import java.awt.Insets
 import java.io.IOException
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JTextArea
+import javax.swing.SwingConstants
+import javax.swing.border.EmptyBorder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+
+val Gray = java.awt.Color(64, 64, 64)
+val DarkGray = java.awt.Color(32, 32, 32)
+val LightGray = java.awt.Color(210, 210, 210)
+
+data class Item(
+    val text: String,
+    val icon: ImageVector,
+    val color: Color,
+    val state: MutableState<Boolean> = mutableStateOf(false)
+)
+val panelItemsList = listOf(
+    Item(text = "Person", icon = Icons.Filled.Person, color = Color(10, 232, 162)),
+    Item(text = "Favorite", icon = Icons.Filled.Favorite, color = Color(150, 232, 150)),
+    Item(text = "Search", icon = Icons.Filled.Search, color = Color(232, 10, 162)),
+    Item(text = "Settings", icon = Icons.Filled.Settings, color = Color(232, 162, 10)),
+    Item(text = "Close", icon = Icons.Filled.Close, color = Color(232, 100, 100))
+)
+val itemSize = 50.dp
+
+fun java.awt.Color.toCompose(): Color {
+    return Color(red, green, blue)
+}
+
 fun main(args: Array<String>) = singleWindowApplication(
     title = "Compose MPP demo",
-    state = WindowState(width = 1024.dp, height = 850.dp),
+    state = WindowState(width = 500.dp, height = 500.dp),
 ) {
-    val app = remember {
-        App(initialScreenName = args.getOrNull(0))
-    }
-    val fontFamilyResolver = LocalFontFamilyResolver.current
-    val fontsLoaded = remember { mutableStateOf(false) }
-
-    if (fontsLoaded.value) {
-        app.Content()
-    }
-
-    LaunchedEffect(Unit) {
-        val fontBytes = getResourceBytes("NotoColorEmoji.ttf")!!
-        val fontFamily = FontFamily(listOf(Font("NotoColorEmoji", fontBytes)))
-        fontFamilyResolver.preload(fontFamily)
-        fontsLoaded.value = true
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = Gray.toCompose()).padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Compose Area", color = LightGray.toCompose())
+        Spacer(modifier = Modifier.height(40.dp))
+        SwingPanel(
+            background = DarkGray.toCompose(),
+            modifier = Modifier.fillMaxSize(),
+            factory = {
+                ComposePanel().apply {
+                    setContent {
+                        Box {
+                            SwingPanel(
+                                modifier = Modifier.fillMaxSize(),
+                                factory = { SwingComponent() }
+                            )
+                            Box (
+                                modifier = Modifier.align(Alignment.TopStart)
+                                    .padding(start = 20.dp, top = 80.dp)
+                                    .background(color = DarkGray.toCompose())
+                            ) {
+                                SwingPanel(
+                                    modifier = Modifier.size(itemSize * panelItemsList.size, itemSize),
+                                    factory = {
+                                        ComposePanel().apply {
+                                            setContent {
+                                                ComposeOverlay()
+                                            }
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        )
     }
 }
 
@@ -60,5 +144,84 @@ suspend fun getResourceBytes(resourceName: String): ByteArray? = withContext(Dis
     } catch (e: IOException) {
         e.printStackTrace()
         return@withContext null
+    }
+}
+
+
+fun SwingComponent() : JPanel {
+    return JPanel().apply {
+        background = DarkGray
+        border = EmptyBorder(20, 20, 20, 20)
+        layout = BorderLayout()
+        add(
+            JLabel("TextArea Swing Component").apply {
+                foreground = LightGray
+                verticalAlignment = SwingConstants.NORTH
+                horizontalAlignment = SwingConstants.CENTER
+                preferredSize = Dimension(40, 160)
+            },
+            BorderLayout.NORTH
+        )
+        add(
+            JTextArea().apply {
+                background = LightGray
+                lineWrap = true
+                wrapStyleWord = true
+                margin = Insets(10, 10, 10, 10)
+                text = "The five boxing wizards jump quickly. " +
+                    "Crazy Fredrick bought many very exquisite opal jewels. " +
+                    "Pack my box with five dozen liquor jugs.\n" +
+                    "Cozy sphinx waves quart jug of bad milk. " +
+                    "The jay, pig, fox, zebra and my wolves quack!"
+            },
+            BorderLayout.CENTER
+        )
+    }
+}
+
+@Composable
+fun ComposeOverlay() {
+    Box(
+        modifier = Modifier.fillMaxSize().
+        background(color = DarkGray.toCompose()),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier.background(
+                shape = RoundedCornerShape(4.dp),
+                color = Color.DarkGray.copy(alpha = 0.5f)
+            )
+        ) {
+            for (item in panelItemsList) {
+                SelectableItem(
+                    text = item.text,
+                    icon = item.icon,
+                    color = item.color,
+                    selected = item.state
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SelectableItem(
+    text: String,
+    icon: ImageVector,
+    color: Color,
+    selected: MutableState<Boolean>
+) {
+    Box(
+        modifier = Modifier.size(itemSize)
+            .clickable { selected.value = !selected.value },
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.alpha(if (selected.value) 1.0f else 0.5f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(modifier = Modifier.size(32.dp), imageVector = icon, contentDescription = null, tint = color)
+            Text(text = text, color = Color.White, fontSize = 10.sp)
+        }
     }
 }
