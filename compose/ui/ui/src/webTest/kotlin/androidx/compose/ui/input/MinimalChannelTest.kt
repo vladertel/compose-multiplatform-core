5f7e07dd3c3e29e5d863a1eb4c500c6c7de32e45
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2024-09-19 15:28:36 +0200

    Introduce MinimalChannelTest

---
+++ compose/ui/ui/src/webTest/kotlin/androidx/compose/ui/input/MinimalChannelTest.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.input
+
+import androidx.compose.ui.sendFromScope
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlinx.browser.window
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.test.runTest
+
+class MinimalChannelTest {
+
+    @Test
+    fun ping() = runTest {
+        val minimalChannel = Channel<Int>(1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+
+        window.setTimeout({
+            minimalChannel.sendFromScope(42)
+            null
+        }, 3000)
+
+        assertEquals(42, minimalChannel.receive())
+    }
+}
