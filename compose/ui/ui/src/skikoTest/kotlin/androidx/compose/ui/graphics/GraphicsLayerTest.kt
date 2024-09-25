vladertel/compose-multiplatform-core

commit 420d0e2389cff9557f76850adf9694856d574e1b

Separate tracking changes inside record scope (#1574)
Fixes https://youtrack.jetbrains.com/issue/CMP-6695

## Testing
Test case from the issue.
This should be tested by QA

## Release Notes
N/A (regression after #1555) wasn't released yet

compose/ui/ui/src/skikoTest/kotlin/androidx/compose/ui/graphics/GraphicsLayerTest.kt
@@ -18,13 +18,9 @@ package androidx.compose.ui.graphics
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.layer.drawLayer
-import androidx.compose.ui.platform.LocalGraphicsContext
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runSkikoComposeUiTest
 import androidx.compose.ui.unit.dp
@@ -34,34 +30,24 @@ import kotlin.test.assertEquals
 @ExperimentalTestApi
 class GraphicsLayerTest {
 
+    // TODO: Add test timeout once available
     @Test // Bug: https://youtrack.jetbrains.com/issue/CMP-6660
-    fun recordInvalidation() = runSkikoComposeUiTest {
-        var backgroundCompositions = 0
-        var foregroundCompositions = 0
+    fun layerDrawingWithRecording() = runSkikoComposeUiTest {
+        var drawCount = 0
         setContent {
-            val graphicsContext = LocalGraphicsContext.current
-            val graphicsLayer = remember { graphicsContext.createGraphicsLayer() }
+            val graphicsLayer = rememberGraphicsLayer()
             Box(Modifier.size(100.dp).drawWithContent {
-                backgroundCompositions++
-                graphicsLayer.record {
-                    this@drawWithContent.drawContent()
-                }
-            })
-            Box(Modifier.size(50.dp).drawBehind {
-                foregroundCompositions++
+                drawCount++
+                graphicsLayer.record { this@drawWithContent.drawContent() }
                 drawLayer(graphicsLayer)
             })
-            DisposableEffect(graphicsLayer) {
-                onDispose { graphicsContext.releaseGraphicsLayer(graphicsLayer) }
-            }
         }
         waitForIdle()
 
-        // UnconfinedTestDispatcher makes some difference here: instead of possible infinite
+        // UnconfinedTestDispatcher might make some difference here: instead of possible infinite
         // invalidation cycle, it resolves invalidations during the same render.
         // But even in this case, with incorrect behaviour composition counters won't be equal 1
 
-        assertEquals(1, backgroundCompositions)
-        assertEquals(1, foregroundCompositions)
+        assertEquals(1, drawCount)
     }
 }

compose/ui/ui-graphics/api/desktop/ui-graphics.api
@@ -993,7 +993,7 @@ public final class androidx/compose/ui/graphics/SkiaColorFilter_skikoKt {
 }
 
 public final class androidx/compose/ui/graphics/SkiaGraphicsContext_skikoKt {
-	public static final fun GraphicsContext ()Landroidx/compose/ui/graphics/GraphicsContext;
+	public static final fun GraphicsContext (Landroidx/compose/runtime/snapshots/SnapshotStateObserver;)Landroidx/compose/ui/graphics/GraphicsContext;
 }
 
 public final class androidx/compose/ui/graphics/SkiaImageAsset_skikoKt {

compose/ui/ui-graphics/build.gradle
@@ -135,7 +135,7 @@ if(AndroidXComposePlugin.isMultiplatformEnabled(project)) {
 
             commonTest {
                 dependencies {
-                    implementation(kotlin("test"))
+                    implementation(libs.kotlinTest)
                 }
             }
 
@@ -163,14 +163,24 @@ if(AndroidXComposePlugin.isMultiplatformEnabled(project)) {
                 implementation(libs.junit)
             }
 
-            desktopTest {
-                resources.srcDirs += "src/desktopTest/res"
+            skikoTest {
+                dependsOn(commonTest)
                 dependencies {
                     implementation(project(":compose:ui:ui-test-junit4"))
+                }
+            }
+
+            desktopTest {
+                dependsOn(skikoTest)
+                resources.srcDirs += "src/desktopTest/res"
+                dependencies {
                     implementation(libs.junit)
                     implementation(libs.truth)
                     implementation(libs.skikoCurrentOs)
                 }
+            }
+            nativeTest {
+                dependsOn(skikoTest)
             }
 
             configureEach {

compose/ui/ui-graphics/src/skikoMain/kotlin/androidx/compose/ui/graphics/SkiaGraphicsContext.skiko.kt
@@ -16,6 +16,7 @@
 
 package androidx.compose.ui.graphics
 
+import androidx.compose.runtime.snapshots.SnapshotStateObserver
 import androidx.compose.ui.InternalComposeUiApi
 import androidx.compose.ui.graphics.layer.GraphicsLayer
 import kotlin.js.JsName
@@ -25,12 +26,14 @@ import kotlin.js.JsName
  */
 @InternalComposeUiApi
 @JsName("createGraphicsContext")
-fun GraphicsContext(): GraphicsContext =
-    SkiaGraphicsContext()
+fun GraphicsContext(snapshotObserver: SnapshotStateObserver): GraphicsContext =
+    SkiaGraphicsContext(snapshotObserver)
 
-private class SkiaGraphicsContext : GraphicsContext {
+private class SkiaGraphicsContext(
+    private val snapshotObserver: SnapshotStateObserver
+) : GraphicsContext {
     override fun createGraphicsLayer(): GraphicsLayer {
-        return GraphicsLayer()
+        return GraphicsLayer(snapshotObserver)
     }
 
     override fun releaseGraphicsLayer(layer: GraphicsLayer) {

compose/ui/ui-graphics/src/skikoMain/kotlin/androidx/compose/ui/graphics/layer/SkiaGraphicsLayer.skiko.kt
@@ -18,13 +18,12 @@ package androidx.compose.ui.graphics.layer
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.neverEqualPolicy
-import androidx.compose.ui.InternalComposeUiApi
+import androidx.compose.runtime.snapshots.SnapshotStateObserver
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.geometry.center
 import androidx.compose.ui.geometry.isUnspecified
 import androidx.compose.ui.geometry.toRect
 import androidx.compose.ui.graphics.BlendMode
@@ -55,12 +54,13 @@ import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
-import kotlin.math.abs
 import org.jetbrains.skia.Picture
 import org.jetbrains.skia.PictureRecorder
 import org.jetbrains.skia.Point3
 
-actual class GraphicsLayer internal constructor() {
+actual class GraphicsLayer internal constructor(
+    private val snapshotObserver: SnapshotStateObserver
+) {
     private val pictureDrawScope = CanvasDrawScope()
     private val pictureRecorder = PictureRecorder()
     private var picture: Picture? = null
@@ -198,9 +198,7 @@ actual class GraphicsLayer internal constructor() {
         } else {
             1.0f
         }
-        childDependenciesTracker.withTracking(
-            onDependencyRemoved = { it.onRemovedFromParentLayer() }
-        ) {
+        trackRecord {
             pictureDrawScope.draw(
                 density,
                 layoutDirection,
@@ -211,6 +209,18 @@ actual class GraphicsLayer internal constructor() {
             )
         }
         picture = pictureRecorder.finishRecordingAsPicture()
+    }
+
+    private fun trackRecord(block: () -> Unit) {
+        childDependenciesTracker.withTracking(
+            onDependencyRemoved = { it.onRemovedFromParentLayer() }
+        ) {
+            snapshotObserver.observeReads(
+                scope = this,
+                onValueChangedForScope = { it.requestDraw() },
+                block = block
+            )
+        }
     }
 
     private fun addSubLayer(graphicsLayer: GraphicsLayer) {

compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/OwnerSnapshotObserver.kt
@@ -25,7 +25,7 @@ import androidx.compose.runtime.snapshots.SnapshotStateObserver
 @Suppress("CallbackName") // TODO rename this and SnapshotStateObserver. b/173401548
 internal class OwnerSnapshotObserver(onChangedExecutor: (callback: () -> Unit) -> Unit) {
 
-    private val observer = SnapshotStateObserver(onChangedExecutor)
+    internal val observer = SnapshotStateObserver(onChangedExecutor)
 
     private val onCommitAffectingLookaheadMeasure: (LayoutNode) -> Unit = { layoutNode ->
         if (layoutNode.isValidOwnerScope) {

compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/node/RootNodeOwner.skiko.kt
@@ -124,6 +124,7 @@ internal class RootNodeOwner(
     val dragAndDropOwner = DragAndDropOwner(platformContext.dragAndDropManager)
 
     private val rootSemanticsNode = EmptySemanticsModifier()
+    private val snapshotObserver = snapshotInvalidationTracker.snapshotObserver()
 
     private val rootModifier = EmptySemanticsElement(rootSemanticsNode)
         .focusProperties {
@@ -162,7 +163,6 @@ internal class RootNodeOwner(
         }
 
     private val rootForTest = PlatformRootForTestImpl()
-    private val snapshotObserver = snapshotInvalidationTracker.snapshotObserver()
     private val pointerInputEventProcessor = PointerInputEventProcessor(owner.root)
     private val measureAndLayoutDelegate = MeasureAndLayoutDelegate(owner.root)
     private var isDisposed = false
@@ -316,7 +316,7 @@ internal class RootNodeOwner(
         override val inputModeManager get() = platformContext.inputModeManager
         override val clipboardManager = PlatformClipboardManager()
         override val accessibilityManager = DefaultAccessibilityManager()
-        override val graphicsContext: GraphicsContext = GraphicsContext()
+        override val graphicsContext = GraphicsContext(this@RootNodeOwner.snapshotObserver.observer)
         override val textToolbar get() = platformContext.textToolbar
         override val autofillTree = AutofillTree()
         override val autofill: Autofill?  get() = null

commit d9d9b3ae1f8e00682b38a9413832e31a60a46213

Fix "Unsupported concurrent change during composition"
Fixes https://youtrack.jetbrains.com/issue/CMP-6729/iOS-Runtime-crash.-Unsupported-concurrent-change-during-composition

Regression after https://github.com/JetBrains/compose-multiplatform-core/pull/1574

The draw call is similar to:
```
// NodeCoordinator.drawBlock
observer.observeReads(Unit, {
  RenderNodeLayer.invalidate()
    drawState.value = Unit
}) {
  RenderNodeLayer.drawLayer()
    drawState.value
}
```

So, triggering the observer in response to the state change also changes this state, which triggers observer again.

It is sometimes manifested via this call:
```
performRecompose()
  val snapshot = Snapshot.takeMutableSnapshot()
    Snapshot.sendApplyNotifications() // triggers drawState.value = Unit
  snapshot.enter {
    ...
    Snapshot.sendApplyNotifications() // can be called anywhere, triggers the observer of `drawState` because we changed `drawState` during the previous `sendApplyNotifications`
  }
  snapshot.apply() // fails with "Unsupported concurrent change during composition"
```
(I haven't figured why sometimes and not always)

Considering this, *we shouldn't change any state we read in `draw` inside `invalidate`*
(because *we shouldn't change any state we read in `observeReads(block=)`) inside observeReads(onValueChangedForScope=)`*)

We added this state not to trigger `RenderNodeLayer.drawLayer` after `RenderNodeLayer.invalidate`, but to trigger `SkiaGraphicsLayer.invalidate` after we called `RenderNodeLayer.drawLayer` inside `SkiaGraphicsLayer.draw`.

Because:
- we still need some way to notify `SkiaGraphicsLayer.invalidate`
- only `RenderNodeLayer.invalidate` knows that `RenderNodeLayer.drawLayer` is changed

We notify via `isDirty` state and a separate check outside `invalidate`

<details>
  <summary>A synthetic reproducer of the issue for history (skip it if details are already understood)</summary>
```
import androidx.compose.runtime.mutableStateOf

compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/ComposeSceneBugsTest.kt
@@ -1,0 +1,96 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.Snapshot
+import androidx.compose.ui.graphics.asComposeCanvas
+import androidx.compose.ui.scene.CanvasLayersComposeScene
+import androidx.compose.ui.scene.ComposeScene
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.skia.Surface
+import org.jetbrains.skiko.MainUIDispatcher
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * Collection of tests for encountered bugs
+ */
+@RunWith(JUnit4::class)
+class ComposeSceneBugsTest {
+    // https://youtrack.jetbrains.com/issue/CMP-6729/iOS-Runtime-crash.-Unsupported-concurrent-change-during-composition
+    // sendApplyNotifications can be called anywhere. When it was called inside composition, it triggers wrongly written observers
+    @Test
+    fun `no crash when sendApplyNotifications performed in composition`() {
+        runLayerSceneTest { scene ->
+            val canvas = Surface.makeRasterN32Premul(100, 100).canvas
+
+            var triggerApplySnapshot by mutableStateOf(false)
+
+            scene.setContent {
+                Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
+                    CircularProgressIndicator()
+                }
+
+                if (triggerApplySnapshot) {
+                    Snapshot.sendApplyNotifications()
+                }
+            }
+
+            repeat(10) {
+                scene.render(canvas.asComposeCanvas(), it * 100L)
+            }
+
+            triggerApplySnapshot = true
+
+            repeat(10) {
+                scene.render(canvas.asComposeCanvas(), 1000 + it * 100L)
+            }
+        }
+    }
+
+    private fun runLayerSceneTest(body: CoroutineScope.(ComposeScene) -> Unit) {
+        var coroutineException: Throwable? = null
+
+        // catching recomposition exceptions this way because of https://youtrack.jetbrains.com/issue/CMP-6734/ComposeScene-doesnt-catch-exceptions-during-recomposition
+        // we catch exceptions this way with real ComposeWindow and the other testing method (runComposeUiTest)
+        runBlocking(
+            MainUIDispatcher + CoroutineExceptionHandler { _, throwable ->
+                coroutineException = throwable
+            }
+        ) {
+            CanvasLayersComposeScene(coroutineContext = coroutineContext).apply {
+                body(this)
+                close()
+            }
+        }
+
+        if (coroutineException != null) {
+            throw coroutineException!!
+        }
+    }
+}

compose/ui/ui/src/skikoTest/kotlin/androidx/compose/ui/graphics/GraphicsLayerTest.kt
@@ -16,23 +16,46 @@
 
 package androidx.compose.ui.graphics
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.layer.drawLayer
+import androidx.compose.ui.platform.LocalGraphicsContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.runSkikoComposeUiTest
+import androidx.compose.ui.test.InternalTestApi
+import androidx.compose.ui.test.SkikoComposeUiTest
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.runInternalSkikoComposeUiTest
 import androidx.compose.ui.unit.dp
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
 
 @ExperimentalTestApi
 class GraphicsLayerTest {
 
-    // TODO: Add test timeout once available
-    @Test // Bug: https://youtrack.jetbrains.com/issue/CMP-6660
-    fun layerDrawingWithRecording() = runSkikoComposeUiTest {
+    // Bug: https://youtrack.jetbrains.com/issue/CMP-6660
+    @Test
+    fun layerDrawingWithRecording() = runLayerTest {
         var drawCount = 0
         setContent {
             val graphicsLayer = rememberGraphicsLayer()
@@ -42,12 +65,181 @@ class GraphicsLayerTest {
                 drawLayer(graphicsLayer)
             })
         }
-        waitForIdle()
 
-        // UnconfinedTestDispatcher might make some difference here: instead of possible infinite
-        // invalidation cycle, it resolves invalidations during the same render.
-        // But even in this case, with incorrect behaviour composition counters won't be equal 1
+        // shouldn't hang
+        awaitIdle()
+    }
 
-        assertEquals(1, drawCount)
+    // Bug: https://youtrack.jetbrains.com/issue/CMP-6695
+    @Test
+    fun invalidateWhenScrollChanged() = runLayerTest {
+        val scrollState = ScrollState(0)
+
+        setContent {
+            val backgroundLayer = rememberGraphicsLayer()
+
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .drawWithContent {
+                        backgroundLayer.record {
+                            this@drawWithContent.drawContent()
+                        }
+                        this@drawWithContent.drawContent()
+                    }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(scrollState)
+                        .fillMaxWidth()
+                ) {
+                    repeat(100) {
+                        Box(Modifier
+                            .size(100.dp)
+                            .background(if (it % 2 == 0) Color.Red else Color.Black))
+                    }
+                }
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .testTag("bar")
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .clip(RectangleShape)
+                    .drawWithContent {
+                        drawLayer(backgroundLayer)
+                    }
+            )
+        }
+
+        awaitIdle()
+        assertEquals(Color.Red, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+
+        scrollState.scrollTo(100)
+        awaitIdle()
+        assertEquals(Color.Black, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+
+        scrollState.scrollTo(200)
+        awaitIdle()
+        assertEquals(Color.Red, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+    }
+
+    // Bug: https://youtrack.jetbrains.com/issue/CMP-6695
+    @Test
+    fun invalidateNestedDrawWhenScrollChanged() = runLayerTest {
+        val scrollState = ScrollState(0)
+
+        setContent {
+            val graphicsContext = LocalGraphicsContext.current
+            val backgroundLayer = rememberGraphicsLayer()
+
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .drawWithContent {
+                        backgroundLayer.record {
+                            this@drawWithContent.drawContent()
+                        }
+                        this@drawWithContent.drawContent()
+                    }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(scrollState)
+                        .fillMaxWidth()
+                ) {
+                    repeat(100) {
+                        Box(Modifier
+                            .size(100.dp)
+                            .background(if (it % 2 == 0) Color.Red else Color.Black))
+                    }
+                }
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .testTag("bar")
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .clip(RectangleShape)
+                    .drawWithContent {
+                        val layer = graphicsContext.createGraphicsLayer()
+                        layer.record { drawLayer(backgroundLayer) }
+                        drawLayer(layer)
+                        graphicsContext.releaseGraphicsLayer(layer)
+                    }
+            )
+        }
+
+        awaitIdle()
+        assertEquals(Color.Red, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+
+        scrollState.scrollTo(100)
+        awaitIdle()
+        assertEquals(Color.Black, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+
+        scrollState.scrollTo(200)
+        awaitIdle()
+        assertEquals(Color.Red, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+    }
+
+    @Test
+    fun invalidateWhenContentChanged() = runLayerTest {
+        var color by mutableStateOf(Color.Red)
+
+        setContent {
+            val backgroundLayer = rememberGraphicsLayer()
+
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .drawWithContent {
+                        backgroundLayer.record {
+                            this@drawWithContent.drawContent()
+                        }
+                        this@drawWithContent.drawContent()
+                    }
+            ) {
+                Box(Modifier.width(100.dp).height(1000.dp).drawWithContent {
+                    drawRect(color, Offset.Zero, Size(100f, 1000f))
+                })
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .testTag("bar")
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .clip(RectangleShape)
+                    .drawWithContent {
+                        drawLayer(backgroundLayer)
+                    }
+            )
+        }
+
+        awaitIdle()
+        assertEquals(Color.Red, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+
+        color = Color.Black
+        awaitIdle()
+        assertEquals(Color.Black, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+
+        color = Color.Red
+        awaitIdle()
+        assertEquals(Color.Red, onNodeWithTag("bar").captureToImage().toPixelMap()[0, 0])
+    }
+
+    @OptIn(InternalTestApi::class)
+    private fun runLayerTest(body: suspend SkikoComposeUiTest.() -> Unit) {
+        runInternalSkikoComposeUiTest(
+            coroutineDispatcher = StandardTestDispatcher()
+        ) {
+            runOnUiThread {
+                runTest(timeout = 10.seconds) {
+                    body()
+                }
+            }
+        }
     }
 }

compose/ui/ui/src/skikoTest/kotlin/androidx/compose/ui/platform/RenderNodeLayerTest.kt
@@ -17,12 +17,19 @@
 package androidx.compose.ui.platform
 
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.Snapshot
+import androidx.compose.runtime.snapshots.SnapshotStateObserver
 import androidx.compose.ui.assertThat
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.layer.GraphicsLayer
 import androidx.compose.ui.isEqualTo
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
 import androidx.compose.ui.unit.*
 import kotlin.math.PI
 import kotlin.math.cos
@@ -511,6 +518,76 @@ class RenderNodeLayerTest {
         childLayer.invalidate()
         parentLayer.drawLayer(canvas, null)
         assertThat(parentDrawCount).isEqualTo(3)
+    }
+
+    // we run in UI thread to not be in concurrent with GlobalSnapshotManager
+    // we have to be non-concurrent with it to not have races with sendApplyNotifications
+    // it is currently hard to isolate Snapshot changes
+    // because SnapshotStateObserver/sendApplyNotifications are coupled with global state
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun invalidate_on_state_change() = runComposeUiTest {
+        runOnUiThread {
+            var drawCount = 0
+            var invalidateCount = 0
+            var state by mutableStateOf(0f)
+            val layer = TestRenderNodeLayer(
+                invalidateParentLayer = {
+                    invalidateCount++
+                },
+                drawBlock = { canvas, _ ->
+                    canvas.drawLine(Offset(0f, state), Offset.Zero, Paint())
+                    drawCount++
+                },
+            )
+
+            val surface = Surface.makeRasterN32Premul(100, 100)
+            val canvas = surface.canvas
+            val stateObserver = SnapshotStateObserver { it.invoke() }
+
+            fun draw() {
+                stateObserver.observeReads(Unit, { layer.invalidate() }) {
+                    layer.drawLayer(canvas.asComposeCanvas(), null)
+                }
+            }
+
+            try {
+                stateObserver.start()
+
+                draw()
+                Snapshot.sendApplyNotifications()
+                assertEquals(0, invalidateCount)
+                assertEquals(1, drawCount)  // no cache, draw
+
+                draw()
+                Snapshot.sendApplyNotifications()
+                assertEquals(0, invalidateCount)
+                assertEquals(1, drawCount) // no draw, as it is cached
+
+                state = 1f
+                Snapshot.sendApplyNotifications()
+                assertEquals(1, invalidateCount)  // invalidate, as draw state changed
+                assertEquals(1, drawCount)
+
+                state = 2f
+                Snapshot.sendApplyNotifications()
+                assertEquals(2, invalidateCount)  // invalidate, as draw state changed again
+                assertEquals(1, drawCount)
+
+                draw()
+                Snapshot.sendApplyNotifications()
+                assertEquals(2, invalidateCount)
+                assertEquals(2, drawCount)  // draw, because we invalidated and cache cleared
+
+                draw()
+                Snapshot.sendApplyNotifications()
+                assertEquals(2, invalidateCount)
+                assertEquals(2, drawCount) // no draw, as it is cached
+            } finally {
+                stateObserver.stop()
+                stateObserver.clear()
+            }
+        }
     }
 
     private fun TestRenderNodeLayer(