06b5b6536aff05ba7d94997c78d3f1a9d79ef6cc
Shalom Gibly <sgibly@google.com>
2024-09-27 15:42:46 -0700

    Fix a DatePicker crash with a future year-range
    Fix a crash at the DatePicker and DateRangePicker when the initial year
    range min-year is in a future year.
    
    Fixes: 319395747
    Test: DatePickerTest and DateRangePickerTest
    Relnote: "Fix crashes in the DatePicker and DateRangePicker when the
    minimum selectable year is set to a future year."
    (cherry picked from https://android-review.googlesource.com/q/commit:88dcf22c37e498d8a72b5d54ed85419cb3b58b18)
    Merged-In: I78656c7fcf507532603e08f4bb4ef34bdba99f87
    Change-Id: I78656c7fcf507532603e08f4bb4ef34bdba99f87

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/DatePicker.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/DatePicker.kt
@@ -1492,7 +1492,9 @@ private fun DatePickerContent(
 ) {
     val displayedMonth = calendarModel.getMonth(displayedMonthMillis)
     val monthsListState =
-        rememberLazyListState(initialFirstVisibleItemIndex = displayedMonth.indexIn(yearRange))
+        rememberLazyListState(
+            initialFirstVisibleItemIndex = displayedMonth.indexIn(yearRange).coerceAtLeast(0)
+        )
     val coroutineScope = rememberCoroutineScope()
     var yearPickerVisible by rememberSaveable { mutableStateOf(false) }
     val defaultLocale = defaultLocale()
