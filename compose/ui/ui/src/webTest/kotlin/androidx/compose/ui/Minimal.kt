3016f67d17d4b75e45a78fb9deca9725e053f80c
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2024-10-09 11:57:33 +0000

    Minimal configuration with failing file

---
+++ compose/ui/ui/src/webTest/kotlin/androidx/compose/ui/Minimal.kt
@@ -0,0 +1,42 @@
+package androidx.compose.ui
+
+import kotlin.test.Test
+import kotlinx.coroutines.test.runTest
+
+class Wtf : OnCanvasTests {
+
+    @Test
+    fun t1() = runTest {
+        createComposeWindow {  }
+    }
+
+    @Test
+    fun t2() = runTest {
+        createComposeWindow {  }
+    }
+
+    @Test
+    fun t3() = runTest {
+        createComposeWindow {  }
+    }
+
+    @Test
+    fun t4() = runTest {
+        createComposeWindow {  }
+    }
+
+    @Test
+    fun t5() = runTest {
+        createComposeWindow {  }
+    }
+
+    @Test
+    fun t6() = runTest {
+        createComposeWindow {  }
+    }
+
+    @Test
+    fun t7() = runTest {
+        createComposeWindow {  }
+    }
+}
