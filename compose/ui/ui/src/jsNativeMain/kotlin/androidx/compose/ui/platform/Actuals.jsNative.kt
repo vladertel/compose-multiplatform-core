ca7a98d8034c89461698bcfe0bcde86b8d82e94b
Ivan Matkov <ivan.matkov@jetbrains.com>
2024-10-09 11:36:33 +0200

    Address feedback: remove a redundant suppression

--- compose/ui/ui/src/jsNativeMain/kotlin/androidx/compose/ui/platform/Actuals.jsNative.kt
+++ compose/ui/ui/src/jsNativeMain/kotlin/androidx/compose/ui/platform/Actuals.jsNative.kt
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-@file:Suppress("ACTUAL_ANNOTATIONS_NOT_MATCH_EXPECT")
-
 package androidx.compose.ui.platform
 
 internal actual fun Any.nativeClass(): Any = this::class
