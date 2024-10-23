017828db70c904d2c50e5cb12b9ef4ff6b27cef7
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2024-10-23 20:03:28 +0300

    Align synchronization declarations between compose modules.

--- compose/runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/platform/Synchronization.kt
+++ compose/runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/platform/Synchronization.kt
@@ -14,6 +14,16 @@
  * limitations under the License.
  */
 
-package androidx.compose.runtime
+package androidx.compose.runtime.platform
 
-internal actual val PTHREAD_MUTEX_ERRORCHECK: Int = platform.posix.PTHREAD_MUTEX_ERRORCHECK
+internal expect class SynchronizedObject
+
+/**
+ * Returns [ref] as a [SynchronizedObject] on platforms where [Any] is a valid [SynchronizedObject],
+ * or a new [SynchronizedObject] instance if [ref] is null or this is not supported on the current
+ * platform.
+ */
+internal expect inline fun makeSynchronizedObject(ref: Any? = null): SynchronizedObject
+
+@PublishedApi
+internal expect inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R
--- compose/runtime/runtime/src/webMain/kotlin/androidx/compose/runtime/platform/Synchronization.web.kt
+++ compose/runtime/runtime/src/webMain/kotlin/androidx/compose/runtime/platform/Synchronization.web.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,11 @@
  * limitations under the License.
  */
 
-package androidx.compose.ui.test
+package androidx.compose.runtime.platform
 
-internal actual inline fun <T> synchronized(lock: Any, block: () -> T) =
-    kotlin.synchronized(lock, block)
+internal actual class SynchronizedObject
+
+internal actual inline fun makeSynchronizedObject(ref: Any?) = SynchronizedObject()
+
+@PublishedApi
+internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R = block()
