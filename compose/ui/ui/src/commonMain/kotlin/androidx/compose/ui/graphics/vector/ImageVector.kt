68d33e0f5bce77dbc88ee18b5adae50de330e331
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2024-10-24 12:28:16 +0300

    Get rid of usage of atomicfu in commonMain in ui module.

--- compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/graphics/vector/ImageVector.kt
+++ compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/graphics/vector/ImageVector.kt
@@ -25,10 +25,9 @@ import androidx.compose.ui.graphics.PathFillType
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.internal.checkPrecondition
+import androidx.compose.ui.platform.createSynchronizedObject
 import androidx.compose.ui.platform.synchronized
 import androidx.compose.ui.unit.Dp
-import kotlinx.atomicfu.locks.SynchronizedObject
-import kotlinx.atomicfu.locks.synchronized
 
 /**
  * Vector graphics object that is generated as a result of [ImageVector.Builder] It can be composed
@@ -379,7 +378,7 @@ internal constructor(
 
     companion object {
         private var imageVectorCount = 0
-        private val sync = SynchronizedObject()
+        private val sync = createSynchronizedObject()
 
         internal fun generateImageVectorId(): Int {
             synchronized(sync) {
