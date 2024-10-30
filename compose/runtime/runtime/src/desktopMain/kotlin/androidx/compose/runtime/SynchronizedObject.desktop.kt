246143f4e3046c44ffc2485596535c5d3f90f02c
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2024-10-30 13:13:04 +0200

    Fix compilation after "Optimize SynchronizedObject allocations on JVM"

--- compose/runtime/runtime/src/desktopMain/kotlin/androidx/compose/runtime/SynchronizedObject.desktop.kt
+++ compose/runtime/runtime/src/desktopMain/kotlin/androidx/compose/runtime/SynchronizedObject.desktop.kt
@@ -22,7 +22,7 @@ package androidx.compose.runtime
 internal actual class SynchronizedObject
 
 @Suppress("NOTHING_TO_INLINE")
-internal actual inline fun makeSynchronizedObject(ref: Any?) = ref ?: SynchronizedObject()
+internal actual inline fun makeSynchronizedObject(ref: Any?) = SynchronizedObject()
 
 @PublishedApi
 internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R =
--- compose/runtime/runtime/src/mingwX64Main/kotlin/androidx/compose/runtime/SynchronizedObject.mingwX64.kt
+++ compose/runtime/runtime/src/mingwX64Main/kotlin/androidx/compose/runtime/SynchronizedObject.mingwX64.kt
@@ -18,6 +18,8 @@ package androidx.compose.runtime
 import androidx.compose.runtime.internal.currentThreadId
 import kotlinx.atomicfu.*
 
+internal actual inline fun makeSynchronizedObject(ref: Any?) = SynchronizedObject()
+
 @PublishedApi
 @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
 internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R {
@@ -42,7 +44,7 @@ internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () 
  * Using a posix mutex is [problematic for mingwX64](https://youtrack.jetbrains.com/issue/KT-70449/Posix-declarations-differ-much-for-mingwX64-and-LinuxDarwin-targets),
  * so we just use a simple spin lock for mingwX64 (maybe reconsidered in case of problems).
  */
-internal actual class SynchronizedObject actual constructor() {
+internal actual class SynchronizedObject {
 
     companion object {
         private const val NO_OWNER = -1L
