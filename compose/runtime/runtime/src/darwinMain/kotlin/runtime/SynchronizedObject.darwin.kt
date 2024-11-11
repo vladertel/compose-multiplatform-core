2efd78e110cd9f81bc98f923f819fdbccc5bec76
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2024-11-11 12:39:51 +0200

    QoS-aware SynchronizedObject experiments: os_unfair_lock version

--- compose/runtime/runtime/src/darwinMain/kotlin/runtime/SynchronizedObject.darwin.kt
+++ compose/runtime/runtime/src/darwinMain/kotlin/runtime/SynchronizedObject.darwin.kt
@@ -16,4 +16,88 @@
 
 package androidx.compose.runtime
 
-internal actual val PTHREAD_MUTEX_ERRORCHECK: Int = platform.posix.PTHREAD_MUTEX_ERRORCHECK
+import kotlin.native.ref.createCleaner
+import kotlinx.cinterop.Arena
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.pointed
+import kotlinx.cinterop.value
+import platform.darwin.os_unfair_lock_lock
+import platform.darwin.os_unfair_lock_t
+import platform.darwin.os_unfair_lock_trylock
+import platform.darwin.os_unfair_lock_unlock
+import platform.darwin.os_unfair_lock
+import platform.darwin.os_unfair_lock_s
+import platform.posix.pthread_cond_t
+import platform.posix.pthread_self
+import platform.posix.pthread_t
+
+internal actual class SynchronizedObject actual constructor() {
+
+    private var owner: pthread_t? = null  // writes are guarded by monitor
+    private var reEnterCount = 0  // only accessed by owner
+    private val monitorWrapper = MonitorWrapper()
+    private val monitor: NativeMonitor get() = monitorWrapper.monitor
+
+    fun lock() {
+        if (owner == pthread_self()) {
+            reEnterCount += 1
+        } else {
+            monitor.enter()
+            owner = pthread_self()
+        }
+    }
+
+    fun unlock() {
+        if (reEnterCount > 0) {
+            reEnterCount -= 1
+        } else {
+            owner = null
+            monitor.exit()
+        }
+    }
+
+    private class MonitorWrapper {
+        val monitor: NativeMonitor = NativeMonitor()
+        val cleaner = createCleaner(monitor, NativeMonitor::dispose)
+    }
+
+
+    @OptIn(ExperimentalForeignApi::class)
+    private class NativeMonitor {
+        private val arena: Arena = Arena()
+
+        private val lock: os_unfair_lock_s = arena.alloc()
+
+        init {
+            // Can't access OS_UNFAIR_LOCK_OPAQUE.
+            lock._os_unfair_lock_opaque = 0u
+        }
+
+        fun enter() {
+            if (!os_unfair_lock_trylock(lock.ptr)) { // Avoid a native blocking call if possible.
+                os_unfair_lock_lock(lock.ptr)
+            }
+        }
+
+        fun exit() = os_unfair_lock_unlock(lock.ptr)
+
+        fun dispose() {
+            arena.clear()
+        }
+    }
+}
+
+@PublishedApi
+@Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
+internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R {
+    lock.run {
+        lock()
+        return try {
+            block()
+        } finally {
+            unlock()
+        }
+    }
+}
--- compose/runtime/runtime/src/linuxMain/kotlin/androidx/compose/runtime/SynchronizedObject.linux.kt
+++ compose/runtime/runtime/src/linuxMain/kotlin/androidx/compose/runtime/SynchronizedObject.linux.kt
@@ -16,4 +16,148 @@
 
 package androidx.compose.runtime
 
-internal actual val PTHREAD_MUTEX_ERRORCHECK: Int = platform.posix.PTHREAD_MUTEX_ERRORCHECK.toInt()
+import androidx.compose.runtime.internal.currentThreadId
+import kotlin.native.ref.createCleaner
+import kotlinx.cinterop.Arena
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.ptr
+import platform.posix.pthread_mutex_destroy
+import platform.posix.pthread_mutex_init
+import platform.posix.pthread_mutex_lock
+import platform.posix.pthread_mutex_t
+import platform.posix.pthread_mutex_unlock
+import platform.posix.pthread_mutexattr_destroy
+import platform.posix.pthread_mutexattr_init
+import platform.posix.pthread_mutexattr_settype
+import platform.posix.pthread_mutexattr_t
+import kotlinx.atomicfu.*
+import kotlinx.atomicfu.AtomicInt
+import platform.posix.pthread_cond_destroy
+import platform.posix.pthread_cond_init
+import platform.posix.pthread_cond_signal
+import platform.posix.pthread_cond_t
+import platform.posix.pthread_cond_wait
+
+internal val PTHREAD_MUTEX_ERRORCHECK: Int = platform.posix.PTHREAD_MUTEX_ERRORCHECK.toInt()
+
+@PublishedApi
+@Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
+internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R {
+    lock.run {
+        lock()
+        return try {
+            block()
+        } finally {
+            unlock()
+        }
+    }
+}
+
+/**
+ * A synchronized object that provides thread-safe locking and unlocking operations.
+ *
+ * `SynchronizedObject` from `kotlinx-atomicfu` library was used before.
+ * However, it is still [experimental](https://github.com/Kotlin/kotlinx-atomicfu?tab=readme-ov-file#locks)
+ * and has [a performance problem](https://github.com/Kotlin/kotlinx-atomicfu/issues/412)
+ * that seriously affects Compose.
+ *
+ * This implementation is optimized for a non-contention case
+ * (that is the case for the current state of Compose for iOS), so it does not create a posix mutex
+ * when there is no contention: using a posix mutex has its own performance overheads.
+ * On the other hand, it does not just spin lock in case of contention,
+ * protecting from an occasional battery drain.
+ */
+internal actual class SynchronizedObject actual constructor() {
+
+    companion object {
+        private const val NO_OWNER = -1L
+    }
+
+    private val owner: AtomicLong = atomic(NO_OWNER)
+    private var reEnterCount: Int = 0
+    private val waiters: AtomicInt = atomic(0)
+
+    private val monitorWrapper: MonitorWrapper by lazy { MonitorWrapper() }
+    private val monitor: NativeMonitor get() = monitorWrapper.monitor
+
+    fun lock() {
+        if (owner.value == currentThreadId()) {
+            reEnterCount += 1
+        } else if (waiters.incrementAndGet() > 1) {
+            waitForUnlockAndLock()
+        } else {
+            if (!owner.compareAndSet(NO_OWNER, currentThreadId())) {
+                waitForUnlockAndLock()
+            }
+        }
+    }
+
+    private fun waitForUnlockAndLock() {
+        withMonitor(monitor) {
+            while (!owner.compareAndSet(NO_OWNER, currentThreadId())) {
+                wait()
+            }
+        }
+    }
+
+    fun unlock() {
+        require (owner.value == currentThreadId())
+        if (reEnterCount > 0) {
+            reEnterCount -= 1
+        } else {
+            owner.value = NO_OWNER
+            if (waiters.decrementAndGet() > 0) {
+                withMonitor(monitor) {
+                    notify()
+                }
+            }
+        }
+    }
+
+    private inline fun withMonitor(monitor: NativeMonitor, block: NativeMonitor.() -> Unit) {
+        monitor.run {
+            enter()
+            return try {
+                block()
+            } finally {
+                exit()
+            }
+        }
+    }
+
+    private class MonitorWrapper {
+        val monitor: NativeMonitor = NativeMonitor()
+        val cleaner = createCleaner(monitor, NativeMonitor::dispose)
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    private class NativeMonitor {
+        private val arena: Arena = Arena()
+        private val cond: pthread_cond_t = arena.alloc()
+        private val mutex: pthread_mutex_t = arena.alloc()
+        private val attr: pthread_mutexattr_t = arena.alloc()
+
+        init {
+            require (pthread_cond_init(cond.ptr, null) == 0)
+            require(pthread_mutexattr_init(attr.ptr) == 0)
+            require (pthread_mutexattr_settype(attr.ptr, PTHREAD_MUTEX_ERRORCHECK) == 0)
+            require(pthread_mutex_init(mutex.ptr, attr.ptr) == 0)
+        }
+
+        fun enter() = require(pthread_mutex_lock(mutex.ptr) == 0)
+
+        fun exit() = require(pthread_mutex_unlock(mutex.ptr) == 0)
+
+        fun wait() = require(pthread_cond_wait(cond.ptr, mutex.ptr) == 0)
+
+        fun notify() = require (pthread_cond_signal(cond.ptr) == 0)
+
+        fun dispose() {
+            pthread_cond_destroy(cond.ptr)
+            pthread_mutex_destroy(mutex.ptr)
+            pthread_mutexattr_destroy(attr.ptr)
+            arena.clear()
+        }
+    }
+}
