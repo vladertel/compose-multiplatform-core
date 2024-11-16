c9e89583e9cf011a083d1f04599a02c1b6eb9625
Igor Demin <igordmn@users.noreply.github.com>
2024-11-16 01:40:51 +0100

    Fix "Context menu on desktop shows incorrect items after the second showing" (#1693)
    Fixes
    https://youtrack.jetbrains.com/issue/CMP-7083/Context-menu-on-desktop-shows-incorrect-items-after-second-showing
    
    The issue was that after the first click we remembered context items,
    but they are constructed dynamically depending on the selected text.
    
    ## Testing
    - the snippet from the issue
    - a new unit test
    
    ## Release Notes
    ### Fixes - Desktop
    - Fix "Context menu on desktop shows incorrect items after the second
    showing"

--- compose/foundation/foundation/src/desktopTest/kotlin/androidx/compose/foundation/ContextMenuTest.kt
+++ compose/foundation/foundation/src/desktopTest/kotlin/androidx/compose/foundation/ContextMenuTest.kt
@@ -16,27 +16,46 @@
 
 package androidx.compose.foundation
 
+import androidx.compose.foundation.copyPasteAndroidTests.lazy.list.assertIsNotPlaced
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalLocalization
+import androidx.compose.ui.platform.PlatformLocalization
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.ComposeUiTest
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performKeyInput
+import androidx.compose.ui.test.performMouseInput
+import androidx.compose.ui.test.performTextInputSelection
+import androidx.compose.ui.test.pressKey
+import androidx.compose.ui.test.rightClick
+import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextRange
 import kotlin.test.assertEquals
-import org.junit.Rule
 import org.junit.Test
 
 
+@OptIn(ExperimentalTestApi::class)
 class ContextMenuTest {
-
-    @get:Rule
-    val rule = createComposeRule()
 
     // https://github.com/JetBrains/compose-multiplatform/issues/2729
     @Test
-    fun `contextMenuArea emits one child when open`() {
+    fun `contextMenuArea emits one child when open`() = runContextMenuTest {
         var childrenCount = 0
 
-        rule.setContent {
+        setContent {
             // We can't just look up the number of children via the semantic node tree because
             // the layout added for the context menu (the empty one in PopupLayout) is not a
             // semantic node
@@ -57,13 +76,13 @@ class ContextMenuTest {
                             )
                         },
                         state = state
-                    ){
+                    ) {
                         Box(content = {})
                     }
                 },
                 measurePolicy = { measurables, _ ->
                     childrenCount = measurables.size
-                    layout(0, 0){}
+                    layout(0, 0) {}
                 }
             )
         }
@@ -71,4 +90,62 @@ class ContextMenuTest {
         assertEquals(1, childrenCount)
     }
 
+    // https://youtrack.jetbrains.com/issue/CMP-7083/Context-menu-on-desktop-shows-incorrect-items-after-the-second-showing
+    @Test
+    fun `different items for different selections in textfield`() = runContextMenuTest {
+        val localization = object : PlatformLocalization {
+            override val copy = "copy"
+            override val cut = "cut"
+            override val paste = "paste"
+            override val selectAll = "selectAll"
+        }
+        val clipboardManager = object : ClipboardManager {
+            override fun setText(annotatedString: AnnotatedString) = Unit
+            override fun getText() = AnnotatedString("text")
+        }
+        setContent {
+            CompositionLocalProvider(
+                LocalLocalization provides localization,
+                LocalClipboardManager provides clipboardManager,
+            ) {
+                BasicTextField("Text", {}, Modifier.testTag("textfield"))
+            }
+        }
+
+        onNodeWithText(localization.copy).assertIsNotPlaced()
+        onNodeWithText(localization.cut).assertIsNotPlaced()
+        onNodeWithText(localization.paste).assertIsNotPlaced()
+        onNodeWithText(localization.selectAll).assertIsNotPlaced()
+
+        onNodeWithTag("textfield").performMouseInput { rightClick() }
+        onNodeWithText(localization.copy).assertIsNotPlaced()
+        onNodeWithText(localization.cut).assertIsNotPlaced()
+        onNodeWithText(localization.paste).isDisplayed()
+        onNodeWithText(localization.selectAll).isDisplayed()
+
+        onNodeWithTag("textfield").performKeyInput { pressKey(Key.Escape) }
+        onNodeWithText(localization.copy).assertIsNotPlaced()
+        onNodeWithText(localization.cut).assertIsNotPlaced()
+        onNodeWithText(localization.paste).assertIsNotPlaced()
+        onNodeWithText(localization.selectAll).assertIsNotPlaced()
+
+        onNodeWithTag("textfield").performTextInputSelection(TextRange(0, "Text".length))
+        onNodeWithTag("textfield").performMouseInput { rightClick() }
+        onNodeWithText(localization.copy).isDisplayed()
+        onNodeWithText(localization.cut).isDisplayed()
+        onNodeWithText(localization.paste).isDisplayed()
+        onNodeWithText(localization.selectAll).assertIsNotPlaced()
+
+        onNodeWithTag("textfield").performKeyInput { pressKey(Key.Escape) }
+        onNodeWithText(localization.copy).assertIsNotPlaced()
+        onNodeWithText(localization.cut).assertIsNotPlaced()
+        onNodeWithText(localization.paste).assertIsNotPlaced()
+        onNodeWithText(localization.selectAll).assertIsNotPlaced()
+    }
+
+    private fun runContextMenuTest(block: ComposeUiTest.() -> Unit) = runComposeUiTest {
+        DesktopPlatform.withOverriddenCurrent(DesktopPlatform.Unknown) {
+            block()
+        }
+    }
 }
