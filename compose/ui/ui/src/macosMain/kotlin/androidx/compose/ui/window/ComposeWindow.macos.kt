e839617b7b4686abb2ed62cf383582f9d79d1ebf
Thomas Vos <thomasjsvos@gmail.com>
2024-12-04 14:07:17 +0100

    Pointer icons for native macOS (#1703)
    Set the correct NSCursor depending on the pointer icon provided by
    compose.
    
    ## Testing
    
    Tested using mpp demo sample on macOS arm64. Open a screen with a text
    field and the cursor changes.

--- compose/ui/ui/src/macosMain/kotlin/androidx/compose/ui/window/ComposeWindow.macos.kt
+++ compose/ui/ui/src/macosMain/kotlin/androidx/compose/ui/window/ComposeWindow.macos.kt
@@ -22,8 +22,10 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.asComposeCanvas
 import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.toComposeEvent
+import androidx.compose.ui.input.pointer.MacosCursor
 import androidx.compose.ui.input.pointer.PointerButton
 import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.platform.MacosTextInputService
 import androidx.compose.ui.platform.PlatformContext
 import androidx.compose.ui.platform.WindowInfoImpl
@@ -44,6 +46,7 @@ import org.jetbrains.skia.Canvas
 import org.jetbrains.skiko.SkiaLayer
 import org.jetbrains.skiko.SkikoRenderDelegate
 import platform.AppKit.NSBackingStoreBuffered
+import platform.AppKit.NSCursor
 import platform.AppKit.NSEvent
 import platform.AppKit.NSTrackingActiveAlways
 import platform.AppKit.NSTrackingActiveInKeyWindow
@@ -93,6 +96,10 @@ private class ComposeWindow(
         object : PlatformContext by PlatformContext.Empty {
             override val windowInfo get() = _windowInfo
             override val textInputService get() = macosTextInputService
+            override fun setPointerIcon(pointerIcon: PointerIcon) {
+                val cursor = (pointerIcon as? MacosCursor)?.cursor ?: NSCursor.arrowCursor
+                cursor.set()
+            }
         }
     private val skiaLayer = SkiaLayer()
     private val scene = CanvasLayersComposeScene(
